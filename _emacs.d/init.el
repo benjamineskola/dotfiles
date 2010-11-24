73dfa97d1c6b70fc42d4ec835346541ab5bd2bc5
Benjamin M. A'Lee <bma@bma.cx>
2010-11-24 23:40:40 +0000

    Split out mail/notmuch config.

--- _emacs.d/init.el
+++ _emacs.d/init.el
@@ -53,33 +53,7 @@
 (setq cssm-mirror-mode nil)
 
 ;; mail
-(or
-  (assoc "mutt-" auto-mode-alist)
-  (setq auto-mode-alist (cons '("mutt-" . mail-mode) auto-mode-alist)))
-
-(add-hook 'mail-mode-hook '(lambda ()
-  (flush-lines "^\\(> \n\\)*> -- \n\\(\n?> .*\\)*") ;;; Kills quoted sigs.
-  (not-modified) ;;; We haven't changed the buffer, haven't we? *g*
-  (mail-text) ;;; Jumps to the beginning of the mail text
-))
-
-;; notmuch
-(add-hook 'message-setup-hook 'mml-secure-sign-pgpmime)
-(add-hook 'message-mode-hook '(lambda () (define-key message-mode-map (kbd "M-TAB") 'bbdb-complete-name)))
-(setq notmuch-fcc-dirs
-       '((".*@mendeley.com" . "mendeley/archive")
-	 (".*" . "archive")))
-(setq notmuch-saved-searches '(
-                       ("inbox" . "tag:inbox AND NOT tag:mendeley")
-                       ("personal" . "tag:personal AND tag:unread")
-                       ("inbox/work" . "tag:mendeley AND tag:inbox AND NOT tag:trac")
-                       ("archive" . "*")
-                       ("archive/work" . "tag:mendeley")
-                       ("trac" . "tag:mendeley AND tag:trac AND tag:unread")
-                       ("archive/trac" . "tag:mendeley AND tag:trac")
-                       ("feeds" . "tag:feeds AND tag:unread")
-                       ("archive/feeds" . "tag:feeds")
-))
+(require 'mail-config)
 
 ;; php
 (add-to-list 'auto-mode-alist '("\\.tpl\\'" . html-mode))

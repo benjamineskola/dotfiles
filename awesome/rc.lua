bd325d7361673b71266b3a4843a03e5b51b107c0
Benjamin M. A'Lee <bma@bma.cx>
2011-01-12 21:21:48 +0000

    Change default layout to be fullscreen.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -13,7 +13,7 @@ beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "xterm"
-editor = editor = os.getenv("EDITOR") or "nano"
+editor =  os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
@@ -46,7 +46,7 @@ layouts =
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, awful.layout.suit.max)
 end
 -- }}}
 

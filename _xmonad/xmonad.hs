07100348b12cc9fb212fa24c6ce97a1686eee58b
Benjamin M. A'Lee <me@benjaminalee.uk>
2011-03-29 11:57:44 +0100

    Manage hook, detecting number of screens.

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -12,6 +12,7 @@ main = do
 		, terminal = "urxvtc"
 		, workspaces = withScreens nScreens (map show ([1..9]++[0]))
 		, keys = myKeys
+		, manageHook = getManageHook nScreens
 		} where
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
@@ -60,3 +61,21 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
         | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+
+getManageHook n = composeAll
+	[ className =? "MPlayer"        --> doFloat
+	, title =? "IRC"	--> doF (W.shift mail)
+	, title =? "Mail"	--> doF (W.shift mail)
+	, title =? "News"	--> doF (W.shift mail)
+	, title =? "identi.ca"	--> doF (W.shift twit)
+	, title =? "twitter"	--> doF (W.shift twit)
+	, className =? "luakit"	--> doF (W.shift web)
+	, className =? "Www"	--> doF (W.shift web)
+	, resource =? "spotify.exe"	--> doF (W.shift music)
+	, resource =? "explorer.exe"	--> doF (W.shift music)
+	] where
+		lastScreen = (show (n-1)) ++ "_"
+		mail = if n == 2 then "1_1" else "0_2"
+		twit = lastScreen ++ "9"
+		web = "0_1"
+		music = "0_0"

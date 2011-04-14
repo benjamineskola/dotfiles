460e4cc6d1a05010847cd64bfe51f7435dcccd2a
Benjamin M. A'Lee <me@benjaminalee.uk>
2011-04-14 16:36:25 +0100

    update and improve ssh configuration.

---
+++ mk.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eu
+
+for i in _ssh/known_hosts.d/*; do cat $i; done > _ssh/known_hosts
+for i in _ssh/*.pub; do cat $i; done > _ssh/authorized_keys
+host=$(hostname|cut -d. -f1)
+ln -sf id_rsa_${host}.pub _ssh/id_rsa.pub
+test -f ~/.private/ssh/id_rsa_${host} && ln -sf ../../.private/ssh/id_rsa_${host} _ssh/id_rsa

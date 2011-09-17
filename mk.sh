89f5445ad8633c8e6702ed8b4fc15348758e7eac
Benjamin M. A'Lee <bma@bma.li>
2011-09-17 13:12:03 +0100

    Stop tracking known_hosts.

--- mk.sh
+++ mk.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-for i in _ssh/known_hosts.d/*; do cat $i; done > _ssh/known_hosts
+#for i in _ssh/known_hosts.d/*; do cat $i; done > _ssh/known_hosts
 for i in _ssh/*.pub; do cat $i; done > _ssh/authorized_keys
 host=$(hostname|cut -d. -f1)
 test -f id_rsa_${host}.pub && ln -sf id_rsa_${host}.pub _ssh/id_rsa.pub

04a8384f67e9429e984b5669c42d08cbadf5cd2d
Benjamin M. A'Lee <me@benjaminalee.uk>
2012-07-17 14:20:29 +0100

    further prompt improvements.

--- .config/prompt.zsh
+++ .config/prompt.zsh
@@ -1,10 +1,9 @@
+autoload -U colors && colors
+
 virtualenv_info() {
 	[ $VIRTUAL_ENV ] && echo "($(basename $VIRTUAL_ENV))"
 }
-collapse_pwd() {
-	echo $(pwd | sed -e "s,^$HOME,~,")
-}
-parse_git_dirty() {
+parse_git_dirt() {
 	local SUBMODULE_SYNTAX=''
 	if [[ $POST_1_7_2_GIT -gt 0 ]]; then
 		SUBMODULE_SYNTAX="--ignore-submodules=dirty"
@@ -20,16 +19,16 @@ git_prompt_info() {
 			return
 		fi
 	fi
-	echo ":${ref#refs/heads/}$(parse_git_dirty)"
+	echo ":%{$fg[magenta]%}${ref#refs/heads/}%{$fg[green]%}$(parse_git_dirty)%{$reset_color%}"
 }
 
-TITLE_PROMPT="[%n@%m:$(collapse_pwd)]"
+TITLE_PROMPT="[%n@%m:${PWD/#$HOME/~}]"
 case $TERM in
 	xterm*|rxvt*|screen*)
 		precmd() {
 			PROMPT="
-%B%n@%m:$(collapse_pwd)$(git_prompt_info)
-$(virtualenv_info)>%b "
+%{$fg[magenta]%}%n%{$reset_color%}@%{$fg[yellow]%}%m%{$reset_color%}:%{$fg[green]%}${PWD/#$HOME/~}%{$reset_color%}$(git_prompt_info)
+$(virtualenv_info)> "
 			print -Pn "\e]0;$TITLE_PROMPT\a";
 		}
 		preexec() {

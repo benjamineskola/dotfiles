4a807d13da0e775ffd4b5b2832aef4a635604ed4
Benjamin M. A'Lee <me@benjaminalee.uk>
2012-10-10 11:56:12 +0100

    check rbenv exists before running.

--- .config/prompt.zsh
+++ .config/prompt.zsh
@@ -2,8 +2,10 @@ autoload -U colors && colors
 
 virtualenv_info() {
 	[ $VIRTUAL_ENV ] && echo "($(basename $VIRTUAL_ENV))"
-	ruby=$(rbenv version 2>/dev/null|cut -d ' ' -f 1)
-	if [ "$ruby" != "system" ]; then echo "($ruby)"; fi
+	if [ -e "$(which rbenv)" ]; then
+		ruby=$(rbenv version 2>/dev/null|cut -d ' ' -f 1)
+		if [ "$ruby" != "system" ]; then echo "($ruby)"; fi
+	fi
 }
 parse_git_dirty() {
 	local SUBMODULE_SYNTAX=''

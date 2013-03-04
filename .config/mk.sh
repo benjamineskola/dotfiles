0bc2301aadb41ad17572a59040b064c243c1af1e
Benjamin M. A'Lee <me@benjaminalee.uk>
2013-03-04 16:32:58 +0000

    remove infra stuff i no longer use.

--- .config/mk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-#for i in _ssh/known_hosts.d/*; do cat $i; done > _ssh/known_hosts
-for i in _ssh/*.pub; do cat $i; done > _ssh/authorized_keys
-host=$(hostname|cut -d. -f1)
-test -f id_rsa_${host}.pub && ln -sf id_rsa_${host}.pub _ssh/id_rsa.pub
-test -f ~/.private/ssh/id_rsa_${host} && ln -sf ../../.private/ssh/id_rsa_${host} _ssh/id_rsa
-exit 0

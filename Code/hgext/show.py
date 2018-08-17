6fc8fac62461b983364153c8660198f5578dc65b
Benjamin Eskola <ben@eskola.me.uk>
2018-08-17 19:42:40 +0100

    Improve parsing of revision specifications.

--- Code/hgext/show.py
+++ Code/hgext/show.py
@@ -1,9 +1,11 @@
 from mercurial import registrar
 from mercurial import commands
+from mercurial import logcmdutil
 
 cmdtable = {}
 command = registrar.command(cmdtable)
 
 @command('show', [], '[revision]')
 def show(ui, repo, revision='.', **opts):
+    revision, differ = logcmdutil.getrevs(repo, [], {'rev':[revision]})
     commands.log(ui, repo, rev=revision, limit=1, patch=True)

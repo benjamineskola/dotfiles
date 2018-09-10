9080d757e3ee1eea0767b1e0f59184f038cfb3d0
Benjamin Eskola <ben@eskola.me.uk>
2018-09-10 06:56:21 +0100

    Remove some superfluous things in the prompt definition.

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,17 +1,17 @@
 case $TERM in
     xterm*|screen*)
-        print -Pn "\e]0;%n@%m:%~\a"
-        preexec () { print -Pn "\e]0;%n@%m:%~> $1\a"; }
+        print -Pn "\e]0;%~\a"
+	preexec () { print -Pn "\e]0;%~ ${1[(w)1]}\a"; }
         ;;
 esac
 
 function vcs_info {
        _vcs_info=''
-       git branch >/dev/null 2>&1 && _vcs_info=" %F{cyan}%F{magenta}${$(git symbolic-ref HEAD)#refs/heads/}%f"
+       git branch >/dev/null 2>&1 && _vcs_info=" git:%F{cyan}${$(git symbolic-ref HEAD)#refs/heads/}%f"
 }
 
 autoload add-zsh-hook
 add-zsh-hook precmd vcs_info
 
 setopt prompt_subst
-PROMPT='[%(!.%F{red}%n .)%f%F{green}${${:-${${${(@j:/:M)${(@s:/:)${(%):-%~}}##.#?}:h}%/}/}#./}${${(%):-%1~}#/}%f${_vcs_info}]%0(?.%F{green}✓.%F{red}✗)%f '
+PROMPT='[%(!.%F{red}%n%f in .)%f%F{green}${${:-${${${(@j:/:M)${(@s:/:)${(%):-%~}}##.#?}:h}%/}/}#./}${${(%):-%1~}#/}%f${_vcs_info}]%0(?.%F{green}✓.%F{red}✗)%f '

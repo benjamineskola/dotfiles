6f58fc154c8dd75506fcbed23c27b0a23a69d2d3
Benjamin Eskola <ben@eskola.me.uk>
2018-10-12 09:43:30 +0100

    Remove unused brews and config

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -4,7 +4,6 @@ alias be='bundle exec'
 alias curl='curl -gkLsS'
 alias dc="dc -e '2k' -f /dev/stdin"
 alias grep=egrep
-alias puppet-lint='puppet-lint --with-filename'
 alias tf="tail -F"
 
 alias ag='ag --pager less --hidden --ignore .git --ignore .hg -i'

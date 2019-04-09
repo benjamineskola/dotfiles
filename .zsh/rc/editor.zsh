a5fecef977d75636bfca2d4697dfa7ec765ee9f6
Ben Eskola <ben@eskola.me.uk>
2019-04-09 13:33:13 +0100

    Switch from neovim to vim 8

--- .zsh/rc/editor.zsh
+++ .zsh/rc/editor.zsh
@@ -1,6 +1,6 @@
-export EDITOR=$(preferred-command nvim vim vi)
+export EDITOR=$(preferred-command vim nvim vi)
 export VISUAL=$EDITOR
 alias vi=$EDITOR
 alias vim=$EDITOR
 
-alias vimr='chruby-exec system -- command vimr'
+alias mvim='mvim --remote-tab-silent'

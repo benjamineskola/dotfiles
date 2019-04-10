d553299c8478376943bae8f3ae87924fb873adcb
Ben Eskola <ben@eskola.me.uk>
2019-04-10 14:26:01 +0100

    Use latest Thoughtbot rubocop config

--- .zsh/rc/ruby.zsh
+++ .zsh/rc/ruby.zsh
@@ -1,1 +1,4 @@
 chruby $(cat ~/.ruby-version)
+
+test -n "$(find ~ -maxdepth 1 -name .rubocop.yml -mtime +1)" &&
+  curl https://raw.githubusercontent.com/thoughtbot/guides/master/style/ruby/.rubocop.yml > .rubocop.yml

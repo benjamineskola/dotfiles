88e4c9f82d83b393bc7674bbfb6b01d3a675dc77
Ben Eskola <ben@eskola.me.uk>
2019-06-18 16:19:17 +0100

    Move vim config inside ~/.config
    Implements https://tlvince.com/vim-respect-xdg

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,6 +20,8 @@ set -gx BAT_THEME OneHalfDark
 
 set -gx HOSTNAME (scutil --get ComputerName)
 
+set -gx VIMINIT 'let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'
+
 mkdir -p $XDG_CACHE_HOME
 
 source /usr/local/share/chruby/chruby.fish

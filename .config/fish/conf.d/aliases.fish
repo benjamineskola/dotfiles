fd5f4cbba85bc9c02d1e81b8465ebd2e3d261fe2
Ben Eskola <ben@eskola.me.uk>
2019-06-18 13:41:34 +0100

    "command" in aliases is unnecessary and breaks completion

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,15 +1,15 @@
-alias curl 'command curl -gkLsS'
-alias dc "command dc -e '2k' -f /dev/stdin"
+alias curl 'curl -gkLsS'
+alias dc "dc -e '2k' -f /dev/stdin"
 alias grep egrep
 alias tf "tail -F"
 
 alias ag rg
-alias rg 'command rg --hidden -g "!.git"'
-alias fetch 'command curl -gkLO'
+alias rg 'rg --hidden -g "!.git"'
+alias fetch 'curl -gkLO'
 
 alias less bat
 
-alias exa 'command exa --git -b'
+alias exa 'exa --git -b'
 alias ls 'exa'
 alias ll 'ls -l'
 alias la 'ls -a'

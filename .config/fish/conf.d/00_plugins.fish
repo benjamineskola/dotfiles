f4e24092e9852de716751d6413f82c8f25bd9ec6
Benjamin Eskola <ben@eskola.me.uk>
2019-06-21 16:43:12 +0100

    Different fish prompt

--- .config/fish/conf.d/00_plugins.fish
+++ .config/fish/conf.d/00_plugins.fish
@@ -2,5 +2,7 @@ if not functions -q fundle
     eval (curl -sfL https://git.io/fundle-install)
 end
 fundle plugin benjamineskola/fish-git
-fundle plugin matchai/spacefish
+fundle plugin rafaelrinaldi/pure
+test -f ~/.config/fish/fundle/rafaelrinaldi/pure/fish_prompt.fish
+and mv ~/.config/fish/fundle/rafaelrinaldi/pure/{*.fish,conf.d/}
 fundle init

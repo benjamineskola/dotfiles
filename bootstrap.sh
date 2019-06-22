c4b94c60635ba6fa0fcd3572440ddafd51f04d1f
Benjamin Eskola <ben@eskola.me.uk>
2019-06-22 12:10:27 +0100

    Add LaunchAgents and Code to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,13 @@
 for i in _* private/_*; do
   ln -sf .config/$i $HOME/.$(basename $i | sed 's/^_//')
 done
+
+test -d LaunchAgents && mkdir -p ~/Library/LaunchAgents
+for i in LaunchAgents/*; do
+  ln -f $HOME/.config/$i $HOME/Library/LaunchAgents
+done
+
+mkdir -p ~/Code
+for i in Code/*; do
+  ln -sf .config/$i $HOME/Code
+done

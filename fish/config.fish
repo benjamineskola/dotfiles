3070f924e1c616bd1e37b22f0f7a9401898e6d65
Benjamin Eskola <ben@eskola.me.uk>
2019-06-23 10:49:34 +0100

    Sort envvars

--- fish/config.fish
+++ fish/config.fish
@@ -1,25 +1,18 @@
-set -gx PATH ~/bin $PATH
-
-set -gx EDITOR vim
-set -gx VISUAL $EDITOR
-
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/Library
 set -gx XDG_CACHE_HOME $HOME/Library/Caches
 
-set -gx BUNDLER_USER_HOME $XDG_CONFIG_HOME/bundler
+set -gx BAT_THEME OneHalfDark
 set -gx BUNDLER_CACHE_HOME $XDG_CACHE_HOME/bundler
-
+set -gx BUNDLER_USER_HOME $XDG_CONFIG_HOME/bundler
+set -gx EDITOR vim
+set -gx HOSTNAME (scutil --get ComputerName)
 set -gx LESS -ImRswFX
 set -gx LESSHISTFILE $XDG_CACHE_HOME/less_history
-
-set -gx BAT_THEME OneHalfDark
-
-set -gx HOSTNAME (scutil --get ComputerName)
-
-set -gx VIMINIT 'let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'
-
+set -gx PATH ~/bin $PATH
 set -gx PYTHONSTARTUP $XDG_CONFIG_HOME/python/startup.py
+set -gx VIMINIT 'let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'
+set -gx VISUAL $EDITOR
 
 mkdir -p $XDG_CACHE_HOME
 

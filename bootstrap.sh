c8196b8f873b05cb09f4cbe48ccceddea0088118
Benjamin Eskola <ben@eskola.me.uk>
2019-06-25 18:16:24 +0100

    Link iterm profiles into place

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,5 +16,7 @@ for i in Code/*; do
   ln -sfh ../.config/$i $HOME/Code
 done
 
+ln -sfhF ../../../.config/iterm2 "$HOME/Library/Application Support/iTerm2/DynamicProfiles"
+
 test -n "$(find $HOME/.config/fish/conf.d -maxdepth 1 -name 01_iterm2_shell_integration.fish -mtime +1)" &&
-  curl https://iterm2.com/shell_integration/fish > $HOME/.config/fish/conf.d/01_iterm2_shell_integration.fish
+  curl https://iterm2.com/shell_integration/fish >$HOME/.config/fish/conf.d/01_iterm2_shell_integration.fish

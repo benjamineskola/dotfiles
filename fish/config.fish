194be2b8b31087002147af8f9472e0c09dcf7dce
Benjamin Eskola <ben@eskola.me.uk>
2019-07-05 13:53:59 +0100

    Split out environment variables
    Use POSIX syntax for reusability. Also remove some redundant ones.

--- fish/config.fish
+++ fish/config.fish
@@ -2,18 +2,6 @@ set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/Library
 set -gx XDG_CACHE_HOME $HOME/Library/Caches
 
-set -gx BUNDLER_CACHE_HOME $XDG_CACHE_HOME/bundler
-set -gx BUNDLER_USER_HOME $XDG_CONFIG_HOME/bundler
-set -gx EDITOR vim
-set -gx HOSTNAME (scutil --get ComputerName)
-set -gx LESS -ImRswFX
-set -gx LESSHISTFILE $XDG_CACHE_HOME/less_history
-set -gx NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npmrc
-set -gx PATH ~/bin $PATH
-set -gx PYTHONSTARTUP $XDG_CONFIG_HOME/python/startup.py
-set -gx VIMINIT 'let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'
-set -gx VISUAL $EDITOR
-
 mkdir -p $XDG_CACHE_HOME
 
 source /usr/local/share/chruby/chruby.fish

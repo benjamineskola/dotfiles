9f8e4d1cfe75a1db309ad19a2ae5653451f51945
Benjamin Eskola <ben@eskola.me.uk>
2019-07-05 14:36:59 +0100

    Only install launchagents and iterm on macos

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,15 +3,17 @@ for i in _* private/_*; do
   ln -sfh .config/$i $HOME/.$(basename $i | sed 's/^_//')
 done
 
-test -d LaunchAgents && mkdir -p ~/Library/LaunchAgents
-for i in LaunchAgents/*.yml; do
-  scripts/yaml2plist.rb $i >$HOME/Library/LaunchAgents/$(basename $i .yml).plist
-done
-for i in LaunchAgents/*.plist; do
-  ln -f $HOME/.config/$i $HOME/Library/LaunchAgents
-done
+if [ "$(uname -s)" = Darwin ]; then
+  test -d LaunchAgents && mkdir -p ~/Library/LaunchAgents
+  for i in LaunchAgents/*.yml; do
+    scripts/yaml2plist.rb $i >$HOME/Library/LaunchAgents/$(basename $i .yml).plist
+  done
+  for i in LaunchAgents/*.plist; do
+    ln -f $HOME/.config/$i $HOME/Library/LaunchAgents
+  done
 
-ln -sfhF ../../../.config/iterm2 "$HOME/Library/Application Support/iTerm2/DynamicProfiles"
+  ln -sfhF ../../../.config/iterm2 "$HOME/Library/Application Support/iTerm2/DynamicProfiles"
+fi
 
 test -n "$(find $HOME/.config/fish/conf.d -maxdepth 1 -name 01_iterm2_shell_integration.fish -mtime +1)" &&
   curl https://iterm2.com/shell_integration/fish >$HOME/.config/fish/conf.d/01_iterm2_shell_integration.fish

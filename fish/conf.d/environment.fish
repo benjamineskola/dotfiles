c7172ef1f2d5bf3dc272cead6904ac0e11881e7e
Benjamin Eskola <ben@eskola.me.uk>
2019-07-08 14:41:26 +0000

    Quote variables to avoid weird behaviour at login (on freebsd only?)

--- fish/conf.d/environment.fish
+++ fish/conf.d/environment.fish
@@ -1,9 +1,9 @@
-export BUNDLER_CACHE_HOME=$XDG_CACHE_HOME/bundler
-export BUNDLER_USER_HOME=$XDG_CONFIG_HOME/bundler
+export BUNDLER_CACHE_HOME="$XDG_CACHE_HOME/bundler"
+export BUNDLER_USER_HOME="$XDG_CONFIG_HOME/bundler"
 export EDITOR=vim
 export LESS=-ImRswFX
-export LESSHISTFILE=$XDG_CACHE_HOME/less_history
-export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npmrc
+export LESSHISTFILE="$XDG_CACHE_HOME/less_history"
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npmrc"
 export PATH="$HOME/bin:$PATH"
-export PYTHONSTARTUP=$XDG_CONFIG_HOME/python/startup.py
+export PYTHONSTARTUP="$XDG_CONFIG_HOME/python/startup.py"
 export VIMINIT='let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'

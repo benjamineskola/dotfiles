ba9657f6fcb2c0c7887f8ab0c30a20201abfe633
Benjamin Eskola <ben@eskola.me.uk>
2019-07-09 13:38:39 +0100

    Install required packages

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -19,3 +19,9 @@ fi
 
 test -n "$(find $HOME/.config/fish/conf.d -maxdepth 1 -name 01_iterm2_shell_integration.fish -mtime +1)" &&
   curl https://iterm2.com/shell_integration/fish >$HOME/.config/fish/conf.d/01_iterm2_shell_integration.fish
+
+command -v brew && brew bundle --global install
+
+command -v bat && bat cache --build
+command -v pip3 && pip3 install -r requirements.txt
+command -v yarn && xargs yarn global add <npm-globals.txt

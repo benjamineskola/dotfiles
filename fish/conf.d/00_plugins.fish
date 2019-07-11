90684c805bca6d7e1bf745da5de700d177555d6a
Benjamin Eskola <ben@eskola.me.uk>
2019-07-11 15:06:01 +0100

    Use fzf with fish integration

--- fish/conf.d/00_plugins.fish
+++ fish/conf.d/00_plugins.fish
@@ -2,5 +2,6 @@ if not functions -q fundle
     eval (curl -sfL https://git.io/fundle-install)
 end
 fundle plugin benjamineskola/fish-git
+fundle plugin jethrokuan/fzf
 fundle plugin matchai/spacefish
 fundle init
---
+++ fish/conf.d/fzf.fish
@@ -0,0 +1,2 @@
+set -g FZF_FIND_FILE_COMMAND "fd --hidden --type f . \$dir"
+set -g FZF_OPEN_COMMAND "fd --hidden --type f . \$dir"

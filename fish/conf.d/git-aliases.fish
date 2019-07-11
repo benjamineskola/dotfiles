b06716f4f1e6cdde5140d5a4569f044a7b8aeb21
Benjamin Eskola <ben@eskola.me.uk>
2019-07-11 08:14:19 +0100

    Stop launching editor for git commit messages

--- fish/conf.d/git-aliases.fish
+++ fish/conf.d/git-aliases.fish
@@ -3,10 +3,4 @@ alias gmv 'git mv'
 alias grm 'git rm'
 alias gpr 'git-pr'
 
-alias gc 'git commit --edit -v'
-alias gc! 'git commit --edit -v --amend'
-alias gca 'git commit --edit -v -a'
-alias gca! 'git commit --edit -v -a --amend'
-alias gcam 'git commit --edit -a -m'
-
 set -x PATH /usr/local/share/git-core/contrib/diff-highlight/ $PATH

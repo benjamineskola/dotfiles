1dee227dbe811a42ad71c67782295ff08965c186
Benjamin Eskola <ben@eskola.me.uk>
2019-08-11 15:49:21 +0100

    suppress some output

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -24,4 +24,4 @@ command -v brew && brew bundle --global install | grep -v '^Using'
 
 command -v bat && bat cache --build
 command -v pip3 && test -x "$(command -v python3)" && pip3 install -r requirements.txt
-command -v yarn && xargs yarn global add <npm-globals.txt
+command -v yarn && xargs yarn global add --silent --no-progress <npm-globals.txt

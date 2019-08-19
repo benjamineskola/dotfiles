6209ad90886cdfcc404dc51a147b8481443806fd
Benjamin Eskola <ben@eskola.uk>
2019-08-19 08:05:58 +0100

    New git aliases

--- fish/conf.d/git-aliases.fish
+++ fish/conf.d/git-aliases.fish
@@ -3,4 +3,17 @@ alias gmv 'git mv'
 alias grm 'git rm'
 alias gpr 'git-pr'
 
+set _git_version (git --version | string match -r '[0-9.]+$' | string split .)
+if test $_git_version[2] -gt 12
+    alias gsw 'git switch'
+    alias grs 'git restore'
+
+    for i in gco gcb gcm gcd
+        alias $i 'echo "Did you mean gsw (git switch) or grs (git restore)?"'
+    end
+
+    alias gswm 'git switch master'
+    alias gswc 'git switch -c'
+end
+
 set -x PATH /usr/local/share/git-core/contrib/diff-highlight/ $PATH

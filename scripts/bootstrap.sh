630d76915260c1386c60036f3477a1b79e6130bd
Benjamin Eskola <ben@eskola.uk>
2019-08-23 10:36:10 +0100

    iterm integration should be the last thing loaded

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -17,8 +17,8 @@ if [ "$(uname -s)" = Darwin ]; then
   ln -sfhF ../../../.config/iterm2 "$HOME/Library/Application Support/iTerm2/DynamicProfiles"
 fi
 
-test -z "$(find "$HOME/.config/fish/conf.d" -maxdepth 1 -name 01_iterm2_shell_integration.fish -mtime -1)" &&
-  curl https://iterm2.com/shell_integration/fish >"$HOME/.config/fish/conf.d/01_iterm2_shell_integration.fish"
+test -z "$(find "$HOME/.config/fish/conf.d" -maxdepth 1 -name zzz_iterm2_shell_integration.fish -mtime -1)" &&
+  curl https://iterm2.com/shell_integration/fish >"$HOME/.config/fish/conf.d/zzz_iterm2_shell_integration.fish"
 
 command -v brew && brew bundle --global install | grep -v '^Using'
 

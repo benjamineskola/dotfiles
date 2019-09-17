7a9b74514e7e79fb7104141480c6a78817bf2348
Benjamin Eskola <ben@eskola.uk>
2019-09-17 09:58:54 +0100

    Exclusions from fzf search

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -1,4 +1,4 @@
-set -g FZF_FIND_FILE_COMMAND "fd --hidden --type f . \$dir"
-set -g FZF_OPEN_COMMAND "fd --hidden --type f . \$dir"
+set -g FZF_FIND_FILE_COMMAND "fd --hidden -E \"{.git,.Trash,Library,Movies,Music,Pictures}\" --type f . \$dir"
+set -g FZF_OPEN_COMMAND "fd --hidden -E \"{.git,.Trash,Library,Movies,Music,Pictures}\" --type f . \$dir"
 
 set -U FZF_LEGACY_KEYBINDINGS 0

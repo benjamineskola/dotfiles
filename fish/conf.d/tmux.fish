6531416e0228c9f8c3c64382c1fd44b21f2dd4d5
Benjamin Eskola <ben@eskola.uk>
2020-02-07 13:34:34 +0000

    tmux/alacritty improvements

--- fish/conf.d/tmux.fish
+++ fish/conf.d/tmux.fish
@@ -1,3 +1,6 @@
-function set_tmux_name --on-event fish_prompt
+function set_tmux_dir_name --on-event fish_prompt
     tmux rename-window (prompt_pwd)
+end
+function set_tmux_command_name --on-event fish_preexec
+    tmux rename-window (string sub -l 20 $argv)
 end

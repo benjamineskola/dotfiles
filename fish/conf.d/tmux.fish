79a581b2ad4d5c2a749ccbb305aa4dda28e6cf4f
Benjamin Eskola <ben@eskola.uk>
2020-02-17 11:29:49 +0000

    Switch back to iterm2

--- fish/conf.d/tmux.fish
+++
@@ -1,6 +0,0 @@
-function set_tmux_dir_name --on-event fish_prompt
-    tmux rename-window (prompt_pwd)
-end
-function set_tmux_command_name --on-event fish_preexec
-    tmux rename-window (string sub -l 20 $argv)
-end

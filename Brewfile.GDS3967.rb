59eb86b9e41426afec3b8104dfc1071bb89eb068
Benjamin Eskola <ben@eskola.uk>
2020-05-04 11:00:39 +0100

    Update brewfiles

--- Brewfile.GDS3967.rb
+++ Brewfile.GDS3967.rb
@@ -1,3 +1,12 @@
+tap "alphagov/gds"
+
 brew "alphagov/gds/gds-cli"
 brew "alphagov/gds/govuk-connect"
+brew "dnsmasq"
+brew "pv"
+brew "terraform-docs"
+brew "tfenv"
+
+cask "1password"
+cask "aws-vault"
 cask "docker"

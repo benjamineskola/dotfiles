1d74f60a357c93fa0db0477ba7c7c903d92c479b
Benjamin Eskola <ben@eskola.uk>
2020-06-17 11:54:28 +0100

    Use standard fish export syntax

--- fish/conf.d/03_environment.fish
+++ fish/conf.d/03_environment.fish
@@ -1,11 +1,11 @@
-export BUNDLE_USER_CACHE="$XDG_CACHE_HOME/bundler"
-export BUNDLE_USER_HOME="$XDG_CONFIG_HOME/bundler"
+set -x BUNDLE_USER_CACHE "$XDG_CACHE_HOME/bundler"
+set -x BUNDLE_USER_HOME "$XDG_CONFIG_HOME/bundler"
 
-export EDITOR=vimr
-export VISUAL=nvim
-export LESS=-ImRswFX
-export LESSHISTFILE="$XDG_CACHE_HOME/less_history"
-export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npmrc"
-export PYTHONSTARTUP="$XDG_CONFIG_HOME/python/startup.py"
+set -x EDITOR vimr
+set -x VISUAL nvim
+set -x LESS -ImRswFX
+set -x LESSHISTFILE "$XDG_CACHE_HOME/less_history"
+set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npmrc"
+set -x PYTHONSTARTUP "$XDG_CONFIG_HOME/python/startup.py"
 
 set fish_user_paths $fish_user_paths ~/bin

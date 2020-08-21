51aca2d2a3c2c472eff0944747bc849e1a21b648
Benjamin Eskola <ben@eskola.uk>
2020-08-21 16:13:54 +0100

    Remove dead links

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,6 +3,7 @@ for i in _* private/_*; do
   test -e "$i" &&
     ln -sfh ".config/$i" "$HOME/.$(basename "$i" | sed 's/^_//')"
 done
+find -L "$HOME" -type l -maxdepth 1 -name '.*'
 
 if [ "$(uname -s)" = Darwin ]; then
   if [ "$(hostname -s)" = Benjamins-MBP ]; then

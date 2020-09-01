e3e8ed52611a9e036b70ec82d74e3718ede24ae1
Benjamin Eskola <ben@eskola.uk>
2020-09-01 14:15:32 +0100

    Set the tmux window title at every prompt

---
+++ fish/conf.d/tmux.fish
@@ -0,0 +1,17 @@
+if test -n "$TMUX"
+    set -x TMUX_WINDOW (tmux display-message -p '#{window_id}')
+    function tmux_pane_rename_cmd -e fish_preexec
+        if string match -vq 'cd*' $argv
+            and string match -vq 'z *' $argv
+            and test 'z' != $argv
+            tmux rename-window -t $TMUX_WINDOW (string replace -r ' .*$' '' $argv)
+        end
+    end
+
+    function tmux_pane_rename_pwd -e fish_prompt
+        tmux rename-window -t $TMUX_WINDOW (basename (string replace "$HOME" '~' $PWD))
+        tmux refresh-client -S
+    end
+
+    tmux_pane_rename_pwd
+end

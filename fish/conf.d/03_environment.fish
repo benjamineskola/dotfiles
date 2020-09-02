7be85ae795ad218a18e911e1204e8aa2be10d047
Benjamin Eskola <ben@eskola.uk>
2020-09-02 11:10:17 +0100

    Use postgres 12

--- fish/conf.d/03_environment.fish
+++ fish/conf.d/03_environment.fish
@@ -10,5 +10,5 @@ set -x PYTHONSTARTUP "$XDG_CONFIG_HOME/python/startup.py"
 set -x GOPATH "$HOME/Code/go"
 
 set fish_user_paths $fish_user_paths ~/bin
-set fish_user_paths $fish_user_paths /Applications/Postgres.app/Contents/Versions/11/bin/
+set fish_user_paths $fish_user_paths /Applications/Postgres.app/Contents/Versions/12/bin
 set fish_user_paths $fish_user_paths $GOPATH/bin

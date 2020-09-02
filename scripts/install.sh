dae609cfe900b37334cc6aaf958acd7a7993489e
Benjamin Eskola <ben@eskola.uk>
2020-09-02 08:53:41 +0100

    Install asdf plugins/versions based on tool-versions

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 set -e
 
+if command -v asdf; then
+  while read lang ver; do
+    if ! asdf plugin-list | grep -q "$lang"; then
+      asdf plugin add "$lang"
+
+      if [ "$lang" = nodejs ]; then
+        bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
+      fi
+    fi
+
+    if ! asdf list $lang | grep -q "^ *$ver$"; then
+      asdf install "$lang" "$ver"
+    fi
+  done <tool-versions
+fi
+
 command -v brew && brew bundle --global install | grep -v '^Using'
 
 command -v pip3 && test -x "$(command -v python3)" && pip3 install --upgrade pip && pip3 install -r requirements.txt | grep -v '^Requirement already satisfied'

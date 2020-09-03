1c61b10f62019d16c2bdfb4f75bb39c9abe1c2e9
Benjamin Eskola <ben@eskola.uk>
2020-09-03 09:41:10 +0100

    Improve patterns for post-checkout matching

--- scripts/post-checkout.sh
+++ scripts/post-checkout.sh
@@ -10,14 +10,14 @@ fi
 changed_files=$(git diff-tree -r --name-only --no-commit-id "$1" "$2")
 
 check_run() {
-  echo "$changed_files" | grep --quiet "$1" && eval "$2" || true
+  echo "$changed_files" | egrep --quiet "$1" && eval "$2" || true
 }
 
-check_run tool-versions ./scripts/install.sh
+check_run '(tool-versions|scripts/)' ./scripts/install.sh
 
 check_run Brewfile ./scripts/install-brew.sh
 check_run Gemfile ./scripts/install-gem.sh
 check_run go-globals.txt ./scripts/install-go.sh
 check_run npm-globals.txt ./scripts/install-npm.sh
-check_run requirements.txt ./scripts/install-pip.sh
+check_run '(requirements.txt|pip/pip.conf)' ./scripts/install-pip.sh
 check_run LaunchAgents ./scripts/install-zzlaunchagents.sh

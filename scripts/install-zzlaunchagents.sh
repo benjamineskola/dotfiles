113e2ece2a983c174ee1eda14da175c211fc36fa
Benjamin Eskola <ben@eskola.uk>
2020-09-03 12:23:23 +0100

    Remove unused launchagent configs

--- scripts/install-zzlaunchagents.sh
+++ scripts/install-zzlaunchagents.sh
@@ -7,9 +7,16 @@ if [ "$(uname -s)" = Darwin ]; then
     for i in LaunchAgents/*.yml; do
       yaml2plist "$i" >"$HOME/Library/LaunchAgents/$(basename "$i" .yml).plist"
     done
+
     for i in LaunchAgents/*.plist; do
       test -e "$i" &&
         ln -f "$HOME/.config/$i" "$HOME/Library/LaunchAgents"
+    done
+
+    for i in "$HOME"/Library/LaunchAgents/*.plist; do
+      if ! [ -f "$HOME/.config/LaunchAgents/$(basename "$i")" -o -f "$HOME/.config/LaunchAgents/$(basename "$i" .plist).yml" ]; then
+        rm "$i"
+      fi
     done
   fi
 fi

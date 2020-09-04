d3c5bf6f66f9ac9c5b83b39cb1dd02a55ab65160
Benjamin Eskola <ben@eskola.uk>
2020-09-04 15:18:19 +0100

    Don't run all install scripts if install.sh itself is run

--- scripts/post-checkout.sh
+++ scripts/post-checkout.sh
@@ -9,23 +9,29 @@ fi
 
 changed_files=$(git diff-tree -r --name-only --no-commit-id "$1" "$2")
 
+has_changed() {
+  echo "$changed_files" | grep -E --quiet "$1"
+}
+
 check_run() {
-  if echo "$changed_files" | grep -E --quiet "$1"; then
+  if has_changed "$1"; then
     eval "$2"
   fi
 }
 
-check_run '(tool-versions|scripts/)' ./scripts/install.sh
+if has_changed '(tool-versions|scripts/)'; then
+  ./scripts/install.sh
+else
+  check_run '(^|/)_.*' ./scripts/install-dotfiles.sh
 
-check_run '(^|/)_.*' ./scripts/install-dotfiles.sh
+  check_run Brewfile ./scripts/install-brew.sh
+  check_run Gemfile ./scripts/install-gem.sh
+  check_run go-globals.txt ./scripts/install-go.sh
+  check_run npm-globals.txt ./scripts/install-npm.sh
+  check_run '(requirements.txt|pip/pip.conf)' ./scripts/install-pip.sh
+  check_run nvim/plugins.vim ./scripts/install-vim.sh
+  check_run LaunchAgents ./scripts/install-zzlaunchagents.sh
 
-check_run Brewfile ./scripts/install-brew.sh
-check_run Gemfile ./scripts/install-gem.sh
-check_run go-globals.txt ./scripts/install-go.sh
-check_run npm-globals.txt ./scripts/install-npm.sh
-check_run '(requirements.txt|pip/pip.conf)' ./scripts/install-pip.sh
-check_run nvim/plugins.vim ./scripts/install-vim.sh
-check_run LaunchAgents ./scripts/install-zzlaunchagents.sh
-
-./scripts/install-bat.sh
-./scripts/install-iterm.sh
+  ./scripts/install-bat.sh
+  ./scripts/install-iterm.sh
+fi

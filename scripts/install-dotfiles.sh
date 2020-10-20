23f537bb06adf5354fd33197caf83113157627e3
Benjamin Eskola <ben@eskola.uk>
2020-10-20 21:35:57 +0100

    can't use find -delete on symlinks so use rm, thx find

--- scripts/install-dotfiles.sh
+++ scripts/install-dotfiles.sh
@@ -5,4 +5,4 @@ for i in _* private/_*; do
   test -e "$i" &&
     ln -sfh ".config/$i" "$HOME/.$(basename "$i" | sed 's/^_//')"
 done
-find -L "$HOME" -type l -maxdepth 1 -name '.*' -delete
+find -L "$HOME" -type l -maxdepth 1 -name '.*' -exec rm {} +

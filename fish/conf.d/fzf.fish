0d34ecfa0b06a2daf9ece7cc54ff0d5501ef8f78
Benjamin Eskola <ben@eskola.uk>
2020-11-20 11:46:24 +0000

    Add a fzf-cd keybinding

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -1,1 +1,2 @@
 bind \eo __fzf_open_file
+bind \ec __fzf_cd
---
+++ fish/functions/__fzf_cd.fish
@@ -0,0 +1,7 @@
+function __fzf_cd --description 'Search the current directory using fzf and fd. Open the file with the default editor.'
+    # Make sure that fzf uses fish to execute __fzf_preview_file.
+    # See similar comment in __fzf_search_shell_variables.fish.
+    set --local --export SHELL (command --search fish)
+    cd (fd --hidden --follow --color=always --exclude "{.git,.Trash,Library,Movies,Music,Pictures}" --type directory . $HOME 2>/dev/null |
+    fzf --ansi --preview='__fzf_preview_file {}')
+end

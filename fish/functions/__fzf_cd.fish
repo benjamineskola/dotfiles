ca51cf246b39c546c2bd37118f6c39c03a1d432f
Benjamin Eskola <ben@eskola.uk>
2020-11-21 16:37:25 +0000

    refresh prompt after changing directory with fzf

--- fish/functions/__fzf_cd.fish
+++ fish/functions/__fzf_cd.fish
@@ -4,4 +4,5 @@ function __fzf_cd --description 'Search the current directory using fzf and fd. 
     set --local --export SHELL (command --search fish)
     cd (fd --hidden --follow --color=always --exclude "{.git,.Trash,Library,Movies,Music,Pictures}" --type directory . $HOME 2>/dev/null |
     fzf --ansi --preview='__fzf_preview_file {}')
+    commandline --function repaint
 end

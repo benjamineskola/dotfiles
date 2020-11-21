22781e871cf8acef232842b7868801ccb5ca8212
Benjamin Eskola <ben@eskola.uk>
2020-11-21 16:36:59 +0000

    Set default heroku app

--- fish/conf.d/03_environment.fish
+++ fish/conf.d/03_environment.fish
@@ -8,6 +8,7 @@ set -x LESSHISTFILE "$XDG_CACHE_HOME/less_history"
 set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npmrc"
 set -x PYTHONSTARTUP "$XDG_CONFIG_HOME/python/startup.py"
 set -x GOPATH "$HOME/Code/go"
+set -x HEROKU_APP eskola-booklogger
 
 set -a fish_user_paths ~/bin
 set -a fish_user_paths /Applications/Postgres.app/Contents/Versions/12/bin

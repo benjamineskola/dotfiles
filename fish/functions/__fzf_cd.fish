989163666e0173c43339a6be3209047f91cfcc12
Benjamin Eskola <ben@eskola.uk>
2020-11-24 10:36:46 +0000

    Make my custom fzf functions submit through commandline function

--- fish/functions/__fzf_cd.fish
+++ fish/functions/__fzf_cd.fish
@@ -1,8 +1,21 @@
-function __fzf_cd --description 'Search the current directory using fzf and fd. Open the file with the default editor.'
+function __fzf_cd --description 'Search the home directory using fzf and fd. Change to the selected directory'
     # Make sure that fzf uses fish to execute __fzf_preview_file.
     # See similar comment in __fzf_search_shell_variables.fish.
     set --local --export SHELL (command --search fish)
-    cd (fd --hidden --follow --color=always --exclude "{.git,.Trash,Library,Movies,Music,Pictures}" --type directory . $HOME 2>/dev/null |
-    fzf --ansi --preview='__fzf_preview_file {}')
-    commandline --function repaint
+
+    set open_status 0
+
+    set dir (fd --hidden --follow --color=always --exclude "{.git,.Trash,Library,Movies,Music,Pictures}" --type directory . $HOME 2>/dev/null | fzf --ansi --preview='__fzf_preview_file {}')
+
+    if test $status -eq 0
+        commandline -r "cd "
+        commandline -a $dir
+
+        commandline -f execute
+        set open_status $status
+    end
+
+    commandline -f repaint
+
+    return $open_status
 end
--- fish/functions/__fzf_open_file.fish
+++ fish/functions/__fzf_open_file.fish
@@ -2,6 +2,22 @@ function __fzf_open_file --description 'Search the current directory using fzf a
     # Make sure that fzf uses fish to execute __fzf_preview_file.
     # See similar comment in __fzf_search_shell_variables.fish.
     set --local --export SHELL (command --search fish)
-    fd --hidden --follow --color=always --exclude=.git --type file 2>/dev/null |
-    fzf --multi --ansi --preview='__fzf_preview_file {}' | xargs $EDITOR
+
+    set open_status 0
+
+    set file_paths_selected (fd --hidden --follow --color=always --exclude "{.git,.Trash,Library,Movies,Music,Pictures}" --type file 2>/dev/null | fzf --multi --ansi --preview='__fzf_preview_file {}')
+
+    if test $status -eq 0
+        commandline -r "$EDITOR "
+        for path in $file_paths_selected
+            set escaped_path (string escape "$path")
+            commandline --insert "$escaped_path "
+        end
+
+        commandline -f execute
+        set open_status $status
+
+    end
+    commandline -f repaint
+    return $open_status
 end

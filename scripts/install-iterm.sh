77decf0fff5be52e7b61680bf51071154bb8f612
Benjamin Eskola <ben@eskola.uk>
2021-03-09 13:56:54 +0000

    Set DynamicProfilesPath for iterm2 rather than flaky symlinks

--- scripts/install-iterm.sh
+++ scripts/install-iterm.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 set -e
 
-if [ "$(uname -s)" = Darwin ]; then
-  mkdir -p "$HOME/Library/Application Support/iTerm2"
-  ln -sfhF ../../../.config/iterm2 "$HOME/Library/Application Support/iTerm2/DynamicProfiles"
-fi
+defaults write com.googlecode.iterm2 DynamicProfilesPath "$HOME/.config/iterm2"
 
 if [ -z "$(find "$HOME/.config/fish/conf.d" -maxdepth 1 -name zzz_iterm2_shell_integration.fish -mtime -1)" ]; then
   curl https://iterm2.com/shell_integration/fish >"$HOME/.config/fish/conf.d/zzz_iterm2_shell_integration.fish"

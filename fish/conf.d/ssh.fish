9ba758f18937514f06c311d2ae3a26944950f124
Benjamin Eskola <ben@eskola.uk>
2021-08-12 15:54:30 +0100

    Use ssh-askpass if it exists and only prompt once

--- fish/conf.d/ssh.fish
+++ fish/conf.d/ssh.fish
@@ -1,3 +1,11 @@
 if status --is-interactive && ! ssh-add -lq >/dev/null
-    ssh-add
+    if ! test -f $HOME/.ssh/askpass-lock
+        touch $HOME/.ssh/askpass-lock
+        if type -q ssh-askpass
+            ssh-add </dev/null
+        else
+            ssh-add
+        end
+        rm $HOME/.ssh/askpass-lock
+    end
 end

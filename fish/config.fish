c865a84566556193ac6d9c0c6a14c39b1678667e
Benjamin Eskola <ben@eskola.uk>
2021-08-19 16:25:22 +0100

    Remove some aliases that are now in fish-git

--- fish/config.fish
+++ fish/config.fish
@@ -72,15 +72,9 @@ if status is-interactive
 
     alias gl glgg
     alias gmv 'git mv'
-    alias grm 'git rm'
     alias gpr git-pr
-    alias grsp 'git restore -p'
-
-    alias gsw 'git switch'
-    alias grs 'git restore'
-
-    alias gswm 'gb | string match -q "* master"; and gsw master; or gsw main'
-    alias gswc 'git switch -c'
+    alias grsp 'grs -p'
+    alias gswm gcm
 
     # alt-shift-. to insert entire previous commandline
     bind \e\> 'commandline -i "$history[1]"'

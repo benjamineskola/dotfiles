1f71fb47d02a4739f4fcf6b52c2ffddb658f9671
Benjamin Eskola <ben@eskola.uk>
2021-09-29 14:25:46 +0100

    EDITOR can safely be mvim and obey alias where it matters

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,9 @@ if status is-interactive
     set -x BUNDLE_USER_CACHE "$XDG_CACHE_HOME/bundler"
     set -x BUNDLE_USER_HOME "$XDG_CONFIG_HOME/bundler"
 
-    set -x EDITOR 'command mvim --remote-silent'
+    set -x EDITOR mvim
     set -x VISUAL vim
-    set -x GIT_EDITOR 'command mvim --remote-wait-silent'
+    set -x GIT_EDITOR 'mvim --remote-wait-silent'
     set -x LESS -ImRswFX
     set -x LESSHISTFILE "$XDG_CACHE_HOME/less_history"
     set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npmrc"

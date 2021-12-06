e110b1040988c7a411368f50a9ae2c7906b50ad6
Benjamin Eskola <ben@eskola.uk>
2021-12-06 15:06:00 +0000

    Ignore global "vim" in lua files

--- nvim/plugin/lsp-config.lua
+++ nvim/plugin/lsp-config.lua
@@ -15,11 +15,28 @@ local servers = {
     "yamlls",
 }
 
+local function on_attach(client, bufnr) end
+local default_opts = {
+    on_attach = on_attach,
+}
+local server_opts = {
+    ["sumneko_lua"] = function()
+        default_opts.settings = {
+            Lua = {
+                diagnostics = {
+                    globals = { "vim" },
+                    disable = { "lowercase-global" },
+                },
+            },
+        }
+    end,
+}
+
 for _, name in pairs(servers) do
     local server_available, requested_server = lsp_installer.get_server(name)
     if server_available then
         requested_server:on_ready(function()
-            local opts = {}
+            local opts = server_opts[name] and server_opts[name]() or default_opts
             requested_server:setup(opts)
         end)
         if not requested_server:is_installed() then

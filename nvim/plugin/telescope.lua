dbef4474596e6fcfb8634ac562b53898f07d6412
Benjamin Eskola <ben@eskola.uk>
2021-12-09 08:33:25 +0000

    Only apply change_directory mapping to find_files picker

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -5,20 +5,29 @@ require("telescope").setup({
             i = {
                 ["<esc>"] = actions.close,
                 ["<C-h>"] = actions.which_key,
-                ["<CR>"] = function(prompt_bufnr)
-                    local path = require("telescope.actions.state").get_selected_entry(prompt_bufnr).value
-                    local f = io.open(path, "r")
-                    local ok, err, code = f:read(1)
-                    f:close()
-                    if code == 21 then -- EISDIR
-                        actions.close(prompt_bufnr)
-                        vim.fn.execute("cd " .. path, "silent")
-                    else
-                        actions.select_default(prompt_bufnr)
-                    end
-                end,
+            },
+        },
+    },
+    pickers = {
+        find_files = {
+            mappings = {
+                i = {
+                    ["<CR>"] = function(prompt_bufnr)
+                        local path = require("telescope.actions.state").get_selected_entry(prompt_bufnr).value
+                        local f = io.open(path, "r")
+                        local ok, err, code = f:read(1)
+                        f:close()
+                        if code == 21 then -- EISDIR
+                            actions.close(prompt_bufnr)
+                            vim.fn.execute("cd " .. path, "silent")
+                        else
+                            actions.select_default(prompt_bufnr)
+                        end
+                    end,
+                },
             },
         },
     },
 })
+
 require("telescope").load_extension("fzf")

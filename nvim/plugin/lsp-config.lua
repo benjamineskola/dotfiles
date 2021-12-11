b0cd4a1e616885332e5d13dc8e6ce0b7ffb34159
Benjamin Eskola <ben@eskola.uk>
2021-12-11 16:22:10 +0000

    Better structure for lsp settings

--- nvim/plugin/lsp-config.lua
+++ nvim/plugin/lsp-config.lua
@@ -2,17 +2,32 @@ local lsp_installer = require("nvim-lsp-installer")
 
 -- Include the servers you want to have installed by default below
 local servers = {
-    "bashls",
-    "cssls",
-    "eslint",
-    "gopls",
-    "hls",
-    "jsonls",
-    "pylsp",
-    "solargraph",
-    "sumneko_lua",
-    "vimls",
-    "yamlls",
+    bashls = {},
+    cssls = {},
+    eslint = {},
+    gopls = {
+        gopls = {
+            gofumpt = true,
+        },
+    },
+    hls = {
+        haskell = {
+            formattingProvider = "brittany",
+        },
+    },
+    jsonls = {},
+    pylsp = {},
+    solargraph = {},
+    sumneko_lua = {
+        Lua = {
+            diagnostics = {
+                globals = { "jit", "vim" },
+                disable = { "lowercase-global" },
+            },
+        },
+    },
+    vimls = {},
+    yamlls = {},
 }
 
 local default_opts = {
@@ -26,47 +41,19 @@ local default_opts = {
         end
     end,
 }
-local server_opts = {
-    ["sumneko_lua"] = function()
-        default_opts.settings = {
-            Lua = {
-                diagnostics = {
-                    globals = { "jit", "vim" },
-                    disable = { "lowercase-global" },
-                },
-            },
-        }
-    end,
-    ["hls"] = function()
-        default_opts.settings = {
-            haskell = {
-                formattingProvider = "brittany",
-            },
-        }
-    end,
-    ["gopls"] = function()
-        default_opts.settings = {
-            gopls = {
-                gofumpt = true,
-            },
-        }
-    end,
-}
 
-for _, name in pairs(servers) do
+for name, settings in pairs(servers) do
     local server_available, requested_server = lsp_installer.get_server(name)
     if server_available then
         requested_server:on_ready(function()
-            local opts = server_opts[name] and server_opts[name]() or default_opts
+            local opts = default_opts
+            opts.settings = settings
             requested_server:setup(opts)
         end)
+
         if not requested_server:is_installed() then
             print("Installing " .. name)
             requested_server:install()
-
-            -- if name == "pylsp" then
-            --     vim.cmd([[PylspInstall pyls-flake8 pylsp-mypy pyls-isort python-lsp-black]])
-            -- end
         end
     end
 end

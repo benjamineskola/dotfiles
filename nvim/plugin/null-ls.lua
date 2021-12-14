b839161c5b2862b90a744115cf6ecfb1dde3acc3
Benjamin Eskola <ben@eskola.uk>
2021-12-14 16:25:55 +0000

    Always use rubocop not standardrb (I don't care for standardrb's choices)
    Also use an external wrapper command to dynamically decide whether to
    use bundler.

--- nvim/plugin/null-ls.lua
+++ nvim/plugin/null-ls.lua
@@ -2,44 +2,13 @@ local nls = require("null-ls")
 
 local b = nls.builtins
 
-local ruby_linter = function(filename, root)
-    local lsputil = require("lspconfig.util")
-    if
-        filename:match("^" .. vim.fn.expand("~/govuk/")) or lsputil.path.exists(lsputil.path.join(root, ".rubocop.yml"))
-    then
-        return "rubocop"
-    else
-        return "standardrb"
-    end
-end
-
-local dynamic_ruby_command = function(params)
-    if not vim.b[params.bufnr].ruby_linter then
-        vim.b[params.bufnr].ruby_linter = ruby_linter(params.bufname, params.root)
-    end
-    return vim.b[params.bufnr].ruby_linter
-end
-
-local dynamic_ruby_args = function(params, module)
-    if dynamic_ruby_command(params) == "rubocop" then
-        return vim.list_extend({ "exec", "rubocop" }, module.rubocop._opts.args)
-    else
-        return module.standardrb._opts.args
-    end
-end
-
 nls.setup({
     sources = {
         -- formatting
         b.formatting.fish_indent, -- fish
         b.formatting.prettier.with({ filetypes = { "html", "json", "yaml", "javascript" } }), -- javascript etc
         b.formatting.stylua, -- lua
-        b.formatting.rubocop.with({
-            dynamic_command = dynamic_ruby_command,
-            args = function(params)
-                return dynamic_ruby_args(params, b.formatting)
-            end,
-        }),
+        b.formatting.rubocop.with({ command = vim.fn.stdpath("config") .. "/helpers/rubocop" }),
         b.formatting.shfmt, -- shell
         b.formatting.shellharden, -- shell
         b.formatting.trim_newlines.with({ disabled_filetypes = { "go", "haskell", "python" } }),
@@ -54,12 +23,7 @@ nls.setup({
                 .. "/bin/luacheck",
             args = vim.list_extend({ "--globals=jit", "--globals=vim" }, b.diagnostics.luacheck._opts.args),
         }), -- lua
-        b.diagnostics.rubocop.with({
-            dynamic_command = dynamic_ruby_command,
-            args = function(params)
-                return dynamic_ruby_args(params, b.diagnostics)
-            end,
-        }),
+        b.diagnostics.rubocop.with({ command = vim.fn.stdpath("config") .. "/helpers/rubocop" }),
         b.diagnostics.shellcheck, -- shell
         b.diagnostics.vint, -- vim
     },

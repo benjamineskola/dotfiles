8d844929df2fc058ca7b9c2ed222d944c802db78
Benjamin Eskola <ben@eskola.uk>
2021-12-17 14:39:11 +0000

    Ensure yapf and autopep8 are never enabled

--- nvim/plugin/lsp-config.lua
+++ nvim/plugin/lsp-config.lua
@@ -21,15 +21,17 @@ local servers = {
     pylsp = {
         pylsp = {
             plugins = {
+                autopep8 = { enabled = false },
                 flake8 = {
                     enabled = true,
                     executable = "flake518",
                 },
+                mccabe = { enabled = false },
                 pycodestyle = { enabled = false },
                 pydocstyle = { enabled = false },
-                mccabe = { enabled = false },
                 pyflakes = { enabled = false },
                 pylint = { enabled = true },
+                yapf = { enabled = false },
             },
         },
     },

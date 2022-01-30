10db9ae0e91dff245aa8ca40c0c3ab9631872504
Benjamin Eskola <ben@eskola.uk>
2022-01-30 15:04:04 +0000

    Improve title function for sudo/caffeinate

--- fish/functions/fish_title.fish
+++ fish/functions/fish_title.fish
@@ -1,6 +1,15 @@
 function fish_title
     if not test $_ = fish -o $_ = cd
-        echo -n "$_: "
+        if not test $_ = sudo -o $_ = caffeinate
+            echo -n "$_: "
+        else
+            for word in (string split ' ' $argv)
+                if ! test "$word" = sudo -o "$word" = caffeinate
+                    echo -n "$word: "
+                    break
+                end
+            end
+        end
     end
     echo (basename (string replace "$HOME" '~' $PWD))
 end

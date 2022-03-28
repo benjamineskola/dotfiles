0c37aa6d5424f792b031c635df1b9179dd1ef24d
Benjamin Eskola <ben@eskola.uk>
2022-03-28 15:03:53 +0100

    Switch to Nova as default editor

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,8 @@
 set -x BUNDLE_USER_CACHE "$XDG_CACHE_HOME/bundler"
 set -x BUNDLE_USER_HOME "$XDG_CONFIG_HOME/bundler"
 
-set -x EDITOR vimr
-set -x VISUAL nvim
+set -x EDITOR nova
+set -x VISUAL "nova -w"
 set -x LESS -ImRswFX
 set -x LESSHISTFILE "$XDG_CACHE_HOME/less_history"
 set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npmrc"

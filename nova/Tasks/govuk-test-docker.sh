4fd8c248fc6db5a2a65027c9e3c18547e52a6eb1
Benjamin Eskola <ben@eskola.uk>
2022-04-06 17:23:53 +0100

    lint

--- nova/Tasks/govuk-test-docker.sh
+++ nova/Tasks/govuk-test-docker.sh
@@ -3,9 +3,9 @@ set -e
 
 test_command='help'
 if grep -q rspec "$PWD"/Gemfile; then
-	test_command='spec'
+  test_command='spec'
 elif grep -q minitest "$PWD"/Gemfile; then
-	test_command='test'
+  test_command='test'
 fi
 
 govuk-docker-run bundle exec rails "$test_command"
--- scripts/install-iterm.sh
+++ scripts/install-iterm.sh
@@ -4,7 +4,7 @@ set -e
 defaults write com.googlecode.iterm2 DynamicProfilesPath "$HOME/.config/iterm2"
 
 if [ -z "$(find "$HOME/.config/fish/conf.d" -maxdepth 1 -name zzz_iterm2_shell_integration.fish -mtime -1)" ]; then
-    curl https://iterm2.com/shell_integration/fish >"$HOME/.config/fish/conf.d/zzz_iterm2_shell_integration.fish"
+  curl https://iterm2.com/shell_integration/fish >"$HOME/.config/fish/conf.d/zzz_iterm2_shell_integration.fish"
 fi
 
 ln -sf ~/.config/scripts/iterm* ~/Library/Application\ Support/iTerm2/Scripts/

29fffa53602af0ad6db0ad9be409bfb591650bee
Benjamin Eskola <ben@eskola.uk>
2022-04-26 21:52:45 +0100

    Use vim as editor for Git
    Switching away from terminal is annoying

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,7 @@ set -x BUNDLE_USER_HOME "$XDG_CONFIG_HOME/bundler"
 
 set -x EDITOR nova
 set -x VISUAL "nova -w"
+set -x GIT_EDITOR vim
 set -x LESS -ImRswFX
 set -x LESSHISTFILE "$XDG_CACHE_HOME/less_history"
 set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npmrc"

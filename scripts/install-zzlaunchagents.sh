3e6e27fe6c6675de0569beaf82fccc62f13073b7
Benjamin Eskola <ben@eskola.uk>
2022-04-27 10:31:43 +0100

    Expand variables in launchagent plists

--- scripts/install-zzlaunchagents.sh
+++ scripts/install-zzlaunchagents.sh
@@ -2,21 +2,17 @@
 set -e
 
 if [ "$(uname -s)" = Darwin ]; then
-  test -d LaunchAgents && mkdir -p ~/Library/LaunchAgents
-  for i in LaunchAgents/*.plist; do
-    if [ -f "$HOME/.config/LaunchAgents/$(basename "$i")" ]; then
-      launchctl unload "$i"
-    fi
-    if [ -f "$i" ]; then
-      ln -f "$HOME/.config/$i" "$HOME/Library/LaunchAgents" &&
+    test -d LaunchAgents && mkdir -p ~/Library/LaunchAgents
+    for i in LaunchAgents/*.plist; do
+        launchctl unload "$HOME/Library/LaunchAgents/$(basename "$i")"
+        sed -E "s,\\\$HOMEBREW_PREFIX,$HOMEBREW_PREFIX,g; s,\\\$HOME,$HOME,g; s,\\\$XDG_CONFIG_HOME,$XDG_CONFIG_HOME,g; s,\\\$XDG_DATA_HOME,$XDG_DATA_HOME,g; s,\\\$XDG_CACHE_HOME,$XDG_CACHE_HOME,g" "$HOME/.config/$i" >"$HOME/Library/LaunchAgents/$(basename "$i")"
         launchctl load "$HOME/Library/LaunchAgents/$(basename "$i")"
-    fi
-  done
+    done
 
-  for i in "$HOME"/Library/LaunchAgents/*.plist; do
-    if ! [ -f "$HOME/.config/LaunchAgents/$(basename "$i")" ]; then
-      launchctl unload "$i"
-      rm "$i"
-    fi
-  done
+    for i in "$HOME"/Library/LaunchAgents/uk.eskola.*.plist; do
+        if ! [ -f "$HOME/.config/LaunchAgents/$(basename "$i")" ]; then
+            launchctl unload "$i"
+            rm "$i"
+        fi
+    done
 fi

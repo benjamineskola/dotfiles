ccf96204c4125b7eb81c2894f67be2250724fe8a
Benjamin Eskola <ben@eskola.uk>
2022-05-01 11:44:41 +0100

    Run through /bin/sh not fish

--- scripts/install-zzlaunchagents.sh
+++ scripts/install-zzlaunchagents.sh
@@ -5,7 +5,7 @@ if [ "$(uname -s)" = Darwin ]; then
     test -d LaunchAgents && mkdir -p ~/Library/LaunchAgents
     for i in LaunchAgents/*.plist; do
         launchctl unload "$HOME/Library/LaunchAgents/$(basename "$i")"
-        sed -E "s,\\\$HOMEBREW_PREFIX,$HOMEBREW_PREFIX,g; s,\\\$HOME,$HOME,g; s,\\\$XDG_CONFIG_HOME,$XDG_CONFIG_HOME,g; s,\\\$XDG_DATA_HOME,$XDG_DATA_HOME,g; s,\\\$XDG_CACHE_HOME,$XDG_CACHE_HOME,g" "$HOME/.config/$i" >"$HOME/Library/LaunchAgents/$(basename "$i")"
+        sed -E "s,\\\$PATH,$PATH,g;  s,\\\$HOMEBREW_PREFIX,$HOMEBREW_PREFIX,g; s,\\\$HOME,$HOME,g; s,\\\$XDG_CONFIG_HOME,$XDG_CONFIG_HOME,g; s,\\\$XDG_DATA_HOME,$XDG_DATA_HOME,g; s,\\\$XDG_CACHE_HOME,$XDG_CACHE_HOME,g" "$HOME/.config/$i" >"$HOME/Library/LaunchAgents/$(basename "$i")"
         launchctl load "$HOME/Library/LaunchAgents/$(basename "$i")"
     done
 

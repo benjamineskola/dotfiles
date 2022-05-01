3984730a306ced2e073260579bb84886c386569b
Benjamin Eskola <ben@eskola.uk>
2022-05-01 11:22:18 +0100

    Enable excludes in autocommit

--- cron.d/hourly/autocommit.sh
+++ cron.d/hourly/autocommit.sh
@@ -3,7 +3,7 @@ set -e
 
 fd -0 -HI '^\.git$' ~/Documents | xargs -0 dirname | while read -r dir; do
     cd "$dir" || continue
-    git ls-files -moz '*.md' | xargs -0 -I{} fish -lc 'mdlint -w "{}"'
+    git ls-files --exclude-standard -moz '*.md' | xargs -0 -I{} fish -lc 'mdlint -w "{}"'
 
     ~/bin/autocommit.sh "$dir"
 done

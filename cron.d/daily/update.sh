3e7aa37c3420243b2ca24fb5eaf22a5265ad3d31
Benjamin Eskola <ben@eskola.uk>
2022-05-03 10:25:42 +0100

    Autoinstall latest python/ruby

--- cron.d/daily/update.sh
+++ cron.d/daily/update.sh
@@ -2,10 +2,16 @@
 set -e
 
 for dir in ~/bin ~/.config; do
-    cd $dir || continue
-    git pull --rebase
+    cd "$dir" || continue
+    git pull --rebase -q || true
 
     cd
 done
 
 brew bundle --global install | grep -v '^Using'
+
+for lang in python ruby; do
+    asdf plugin-update "$lang"
+    version=$(asdf latest "$lang")
+    asdf install "$lang" "$version" && asdf global "$lang" "$version" && asdf reshim "$lang"
+done

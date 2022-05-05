67fbf9ea5d7aedbcc942667ca28e2c2112275590
Benjamin Eskola <ben@eskola.uk>
2022-05-05 21:36:27 +0100

    Exclude Notes to avoid duplication

--- cron.d/hourly/autocommit.sh
+++ cron.d/hourly/autocommit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-fd -HI '^\.git$' ~/Documents -X dirname | while read -r dir; do
+fd -HI '^\.git$' ~/Documents -E 'Notes/' -X dirname | while read -r dir; do
     cd "$dir" || continue
     git ls-files --exclude-standard -moz '*.md' | xargs -0 -I{} fish -lc 'mdlint -w "{}"'
 

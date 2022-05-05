d05febbc414ff7bd39ae5aa79c51bdf522010f3b
Benjamin Eskola <ben@eskola.uk>
2022-05-05 21:34:59 +0100

    Better execution of dirname

--- cron.d/hourly/autocommit.sh
+++ cron.d/hourly/autocommit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-fd -0 -HI '^\.git$' ~/Documents | xargs -0 dirname | while read -r dir; do
+fd -HI '^\.git$' ~/Documents -X dirname | while read -r dir; do
     cd "$dir" || continue
     git ls-files --exclude-standard -moz '*.md' | xargs -0 -I{} fish -lc 'mdlint -w "{}"'
 

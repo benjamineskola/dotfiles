364716a0fb951cc25d20c10bda0a07e5c8df085d
Benjamin Eskola <ben@eskola.uk>
2022-05-10 17:08:36 +0100

    Reduce duplication of code in __fzf_cd

--- fish/functions/__fzf_cd.fish
+++ fish/functions/__fzf_cd.fish
@@ -1,21 +1,9 @@
 function __fzf_cd --description 'Search the home directory using fzf and fd. Change to the selected directory'
-    # Make sure that fzf uses fish to execute __fzf_preview_file.
-    # See similar comment in __fzf_search_shell_variables.fish.
-    set --local --export SHELL (command --search fish)
-
     set open_status 0
 
-    set dir (fd --hidden --follow --color=always --exclude "{.git,.Trash,Library,Movies,Music,Pictures}" --type directory . $HOME 2>/dev/null | fzf --ansi --preview='exa -G {}')
+    set --local -x fzf_dir_opts "--bind=enter:accept"
+    set --local -a -x fzf_fd_opts --type d
 
-    if test $status -eq 0
-        commandline -r "cd "
-        commandline -a (string escape "$dir")
-
-        commandline -f execute
-        set open_status $status
-    end
-
-    commandline -f repaint
-
-    return $open_status
+    _fzf_search_directory
+    commandline -f execute
 end

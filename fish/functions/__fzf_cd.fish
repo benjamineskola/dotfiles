42266690b2b099fee43eff485805d08c692f2277
Benjamin Eskola <ben@eskola.uk>
2022-05-11 20:17:30 +0100

    Use Z for fzf-cd to get recency-ordering

--- fish/functions/__fzf_cd.fish
+++ fish/functions/__fzf_cd.fish
@@ -1,9 +1,7 @@
-function __fzf_cd --description 'Search the home directory using fzf and fd. Change to the selected directory'
-    set open_status 0
-
-    set --local -x fzf_dir_opts "--bind=enter:accept"
-    set --local -a -x fzf_fd_opts --type d
-
-    _fzf_search_directory
-    commandline -f execute
+function __fzf_cd --description 'Search the home directory using fzf and z. Change to the selected directory'
+    set dir (z -l | cut -w -f 2 | fzf --preview 'exa -G {}')
+    if [ $status -eq 0 ]
+        z "$dir"
+    end
+    commandline -f repaint
 end

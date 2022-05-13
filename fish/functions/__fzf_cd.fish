d85e2bdbd6d4ef39ebc36688e10d77b7b944a572
Benjamin Eskola <ben@eskola.uk>
2022-05-13 15:02:31 +0100

    Improved fzf options in fzf_cd

--- fish/functions/__fzf_cd.fish
+++ fish/functions/__fzf_cd.fish
@@ -1,5 +1,5 @@
 function __fzf_cd --description 'Search the home directory using fzf and z. Change to the selected directory'
-    set dir (z -l | cut -w -f 2 | fzf --preview 'exa -G {}')
+    set dir (z -l | awk '{print $2}' | fzf --preview 'exa -G {}' --cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*")
     if [ $status -eq 0 ]
         z "$dir"
     end

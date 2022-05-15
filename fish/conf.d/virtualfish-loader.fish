195bcaa9ef916e3dacccab5dbf064278822df06f
Benjamin Eskola <ben@eskola.uk>
2022-05-15 11:48:55 +0100

    Use virtualfish

---
+++ fish/conf.d/virtualfish-loader.fish
@@ -0,0 +1,6 @@
+set -g VIRTUALFISH_HOME $XDG_DATA_HOME/virtulenvs
+set -g VIRTUALFISH_VERSION 2.5.4
+set -g VIRTUALFISH_PYTHON_EXEC /Users/ben/Library/asdf/shims/python
+source /Users/ben/Library/asdf/installs/python/3.10.4/lib/python3.10/site-packages/virtualfish/virtual.fish
+source /Users/ben/Library/asdf/installs/python/3.10.4/lib/python3.10/site-packages/virtualfish/auto_activation.fish
+emit virtualfish_did_setup_plugins

660d211f91f3a8403ad207b3cd1b6cb5a316f4eb
Benjamin Eskola <ben@eskola.uk>
2022-05-24 14:26:19 +0100

    Ensure latest pip installed in all virtualenvs

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -1,6 +1,9 @@
+grep -q '^pip$' $VIRTUALFISH_HOME/global_requirements.txt || echo pip >>$VIRTUALFISH_HOME/global_requirements.txt
+
 set -g VIRTUALFISH_HOME $XDG_DATA_HOME/virtulenvs
 set -g VIRTUALFISH_VERSION 2.5.4
 set -g VIRTUALFISH_PYTHON_EXEC /Users/ben/Library/asdf/shims/python
 source /Users/ben/Library/asdf/installs/python/3.10.4/lib/python3.10/site-packages/virtualfish/virtual.fish
+source /Users/benjamineskola/Library/asdf/installs/python/3.10.4/lib/python3.10/site-packages/virtualfish/global_requirements.fish
 source /Users/ben/Library/asdf/installs/python/3.10.4/lib/python3.10/site-packages/virtualfish/auto_activation.fish
 emit virtualfish_did_setup_plugins

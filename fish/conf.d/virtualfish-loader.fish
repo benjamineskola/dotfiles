c14a1dbc6616375d117eadce1ce7496e880b1d72
Benjamin Eskola <ben@eskola.uk>
2022-05-25 21:42:49 +0100

    Fix virtualfish paths

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -1,9 +1,9 @@
+set -g VIRTUALFISH_HOME $XDG_DATA_HOME/virtulenvs
 grep -q '^pip$' $VIRTUALFISH_HOME/global_requirements.txt || echo pip >>$VIRTUALFISH_HOME/global_requirements.txt
 
-set -g VIRTUALFISH_HOME $XDG_DATA_HOME/virtulenvs
 set -g VIRTUALFISH_VERSION 2.5.4
-set -g VIRTUALFISH_PYTHON_EXEC /Users/ben/Library/asdf/shims/python
-source /Users/ben/Library/asdf/installs/python/3.10.4/lib/python3.10/site-packages/virtualfish/virtual.fish
-source /Users/benjamineskola/Library/asdf/installs/python/3.10.4/lib/python3.10/site-packages/virtualfish/global_requirements.fish
-source /Users/ben/Library/asdf/installs/python/3.10.4/lib/python3.10/site-packages/virtualfish/auto_activation.fish
+set -g VIRTUALFISH_PYTHON_EXEC $HOME/Library/asdf/shims/python
+source $HOME/Library/asdf/installs/python/*/lib/python3.*/site-packages/virtualfish/virtual.fish
+source $HOME/Library/asdf/installs/python/*/lib/python3.*/site-packages/virtualfish/global_requirements.fish
+source $HOME/Library/asdf/installs/python/*/lib/python3.*/site-packages/virtualfish/auto_activation.fish
 emit virtualfish_did_setup_plugins

73c778163ff871f10c8aa5b3f0f7902720fefdc8
Benjamin Eskola <ben@eskola.uk>
2022-07-10 20:37:40 +0100

    Correct XDG dirs on non-Darwin

--- fish/conf.d/00_xdg.fish
+++ fish/conf.d/00_xdg.fish
@@ -1,5 +1,11 @@
 set -gx XDG_CONFIG_HOME $HOME/.config
-set -gx XDG_DATA_HOME $HOME/Library
-set -gx XDG_CACHE_HOME $HOME/Library/Caches
+
+if [ (uname -s) = Darwin ]
+    set -gx XDG_DATA_HOME $HOME/Library
+    set -gx XDG_CACHE_HOME $HOME/Library/Caches
+else
+    set -gx XDG_DATA_HOME $HOME/.local
+    set -gx XDG_CACHE_HOME $HOME/.cache
+end
 
 mkdir -p $XDG_CACHE_HOME

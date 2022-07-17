9874d899ec8109770e419d6c36bcda86f02bf4cd
Benjamin Eskola <ben@eskola.uk>
2022-07-17 12:40:13 +0100

    Function for doing wordcounts on markdown files using pandoc and wordcount.lua

---
+++ fish/functions/mdwc.fish
@@ -0,0 +1,16 @@
+function mdwc
+    set total 0
+    for file in $argv
+        set tmpfile (mktemp)
+        trap "rm -f '$tmpfile'" EXIT INT
+
+        perl -0777 -pe 's/^---+\n(\n|.)*?\n---+\n+//' "$file" >"$tmpfile"
+
+        set count (pandoc -L ~/Code/lua-filters/wordcount/wordcount.lua "$tmpfile" | grep words | cut -d ' ' -f 1)
+        set total (math $count + $total)
+        printf "%8d %s\n" $count $file
+
+        rm -f "$tmpfile"
+    end
+    printf "%8d total\n" $total
+end

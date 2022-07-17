c983be03f176e1e54a8c62f8fee0cf4c0129110b
Benjamin Eskola <ben@eskola.uk>
2022-07-17 09:49:32 +0100

    Better virtualfish loader

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -1,11 +1,12 @@
-set -g VIRTUALFISH_HOME $XDG_DATA_HOME/virtualenvs
+set -gx VIRTUALFISH_HOME $XDG_DATA_HOME/virtualenvs
 mkdir -p $VIRTUALFISH_HOME
 
 ln -sf $XDG_CONFIG_HOME/vf-requirements $VIRTUALFISH_HOME/global_requirements.txt
 
 set -g VIRTUALFISH_VERSION 2.5.4
 set -g VIRTUALFISH_PYTHON_EXEC $HOME/Library/asdf/shims/python
-source $HOME/Library/asdf/installs/python/*/lib/python3.*/site-packages/virtualfish/virtual.fish
-source $HOME/Library/asdf/installs/python/*/lib/python3.*/site-packages/virtualfish/global_requirements.fish
-source $HOME/Library/asdf/installs/python/*/lib/python3.*/site-packages/virtualfish/auto_activation.fish
+set -g SITE_PACKAGES (find ~/Library/asdf/installs/python/ -name site-packages -prune)
+source $SITE_PACKAGES/virtualfish/virtual.fish
+source $SITE_PACKAGES/virtualfish/global_requirements.fish
+source $SITE_PACKAGES/virtualfish/auto_activation.fish
 emit virtualfish_did_setup_plugins

ebb31f2b1ff5be69bf16cd377c9746517d99050f
Benjamin Eskola <ben@eskola.uk>
2022-10-17 14:55:35 +0100

    Drop support for Intel macs

--- fish/conf.d/01_homebrew.fish
+++ fish/conf.d/01_homebrew.fish
@@ -1,10 +1,5 @@
 if [ $OS = Darwin ]
-    if [ $ARCH = arm ]
-        set HOMEBREW_PREFIX /opt/homebrew
-    else
-        set HOMEBREW_PREFIX /usr/local
-    end
-    eval ($HOMEBREW_PREFIX/bin/brew shellenv)
+    eval (/opt/homebrew/bin/brew shellenv)
 end
 
 set -gx HOMEBREW_BUNDLE_FILE $XDG_CONFIG_HOME/Brewfile

49e8a77dbd67c6a10266dd18e214850ede193e3f
Benjamin Eskola <ben@eskola.uk>
2022-11-01 14:03:15 +0000

    Use possibly bundled rubocop through null-ls

--- nvim/plugin/null-ls.lua
+++ nvim/plugin/null-ls.lua
@@ -5,8 +5,32 @@ local b = nls.builtins
 local function has_rubocop(utils)
     return utils.root_has_file(".rubocop.yml")
 end
+
 local function hasnt_rubocop(utils)
     return not has_rubocop(utils)
+end
+
+local function bundled_rubocop()
+    local utils = require("null-ls.utils").make_conditional_utils()
+    local gemfile = require("null-ls.utils").get_root() .. "/Gemfile"
+    if utils.root_has_file("Gemfile") then
+        return os.execute("grep -q rubocop " .. gemfile) == 0
+    end
+    return false
+end
+
+local function rubocop_args(type)
+    local cmd = "rubocop"
+    local args = type.rubocop._opts.args
+    if bundled_rubocop() then
+        cmd = "bundle"
+        args = vim.list_extend({ "exec", "rubocop" }, args)
+    end
+    return {
+        condition = has_rubocop,
+        command = cmd,
+        args = args,
+    }
 end
 
 nls.setup({
@@ -16,11 +40,7 @@ nls.setup({
         b.formatting.fish_indent, -- fish
         b.formatting.prettier.with({ filetypes = { "json", "yaml" } }),
         b.formatting.stylua, -- lua
-        b.formatting.rubocop.with({
-            condition = has_rubocop,
-            command = "bundle",
-            args = vim.list_extend({ "exec", "rubocop" }, nls.builtins.formatting.rubocop._opts.args),
-        }),
+        b.formatting.rubocop.with(rubocop_args(b.formatting)),
         b.formatting.standardrb.with({ condition = hasnt_rubocop }),
         b.formatting.shfmt, -- shell
         b.formatting.trim_newlines.with({ disabled_filetypes = { "go", "haskell", "python" } }),
@@ -36,11 +56,7 @@ nls.setup({
                 .. "/bin/luacheck",
             args = vim.list_extend({ "--globals=jit", "--globals=vim" }, b.diagnostics.luacheck._opts.args),
         }), -- lua
-        b.diagnostics.rubocop.with({
-            condition = has_rubocop,
-            command = "bundle",
-            args = vim.list_extend({ "exec", "rubocop" }, nls.builtins.formatting.rubocop._opts.args),
-        }),
+        b.diagnostics.rubocop.with(rubocop_args(b.diagnostics)),
         b.diagnostics.standardrb.with({ condition = hasnt_rubocop }),
         b.diagnostics.shellcheck, -- shell
         b.diagnostics.tsc, -- typescript

ec3a8828ccab5bf4b5bd604a8c892a3e9cc4b77f
Benjamin Eskola <ben@eskola.uk>
2022-11-06 22:01:44 +0000

    Fancy completion menu in nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -37,6 +37,32 @@ return require("packer").startup(function(use)
         end,
     })
     use({
+        "gelguy/wilder.nvim",
+        config = function()
+            local wilder = require("wilder")
+            wilder.setup({ modes = { ":", "/", "?" } })
+            wilder.set_option(
+                "renderer",
+                wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+                    border = "rounded",
+                    highlighter = wilder.lua_fzy_highlighter(),
+                    left = { " ", wilder.popupmenu_devicons() },
+                    right = { " ", wilder.popupmenu_scrollbar() },
+                }))
+            )
+            wilder.set_option("pipeline", {
+                wilder.branch(
+                    wilder.cmdline_pipeline({
+                        fuzzy = 1,
+                        fuzzy_filter = wilder.lua_fzy_filter(),
+                    }),
+                    wilder.vim_search_pipeline()
+                ),
+            })
+        end,
+        requires = { "nvim-tree/nvim-web-devicons", "romgrk/fzy-lua-native" },
+    })
+    use({
         "hrsh7th/nvim-cmp",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",

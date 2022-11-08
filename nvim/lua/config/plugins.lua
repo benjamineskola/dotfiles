b9962c9f6e4bcee228854d4714f606f10b828e7c
Benjamin Eskola <ben@eskola.uk>
2022-11-08 18:16:23 +0000

    Move config out to separate file

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -38,28 +38,6 @@ return require("packer").startup(function(use)
     })
     use({
         "gelguy/wilder.nvim",
-        config = function()
-            local wilder = require("wilder")
-            wilder.setup({ modes = { ":", "/", "?" } })
-            wilder.set_option(
-                "renderer",
-                wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-                    border = "rounded",
-                    highlighter = wilder.lua_fzy_highlighter(),
-                    left = { " ", wilder.popupmenu_devicons() },
-                    right = { " ", wilder.popupmenu_scrollbar() },
-                }))
-            )
-            wilder.set_option("pipeline", {
-                wilder.branch(
-                    wilder.cmdline_pipeline({
-                        fuzzy = 1,
-                        fuzzy_filter = wilder.lua_fzy_filter(),
-                    }),
-                    wilder.vim_search_pipeline()
-                ),
-            })
-        end,
         requires = { "nvim-tree/nvim-web-devicons", "romgrk/fzy-lua-native" },
     })
     use({

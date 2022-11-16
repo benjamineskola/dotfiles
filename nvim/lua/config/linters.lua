635ad8f5b59b7273ca8e26b5c1ab6e0204a76038
Benjamin Eskola <ben@eskola.uk>
2022-11-16 08:30:05 +0000

    Load lint module rather than repeatedly requiring

--- nvim/lua/config/linters.lua
+++ nvim/lua/config/linters.lua
@@ -1,8 +1,10 @@
+local lint = require("lint")
+
 local ruby_linter = "standardrb"
 if require("utils").has_rubocop() then ruby_linter = "rubocop" end
 
-local standardrb = require("lint").linters.standardrb
-require("lint").linters.standardrb = function()
+local standardrb = lint.linters.standardrb
+lint.linters.standardrb = function()
     standardrb.ignore_exitcode = true
     return standardrb
 end
@@ -33,14 +35,14 @@ require("lint").linters_by_ft = {
 }
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufWritePost" }, {
-    callback = function() require("lint").try_lint() end,
+    callback = function() lint.try_lint() end,
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
     pattern = { "ruby", "typescript" },
     callback = function()
         vim.api.nvim_create_autocmd({ "TextChanged" }, {
-            callback = function() require("lint").try_lint() end,
+            callback = function() lint.try_lint() end,
             group = vim.api.nvim_create_augroup("lint_ft_textchanged", { clear = true }),
         })
     end,

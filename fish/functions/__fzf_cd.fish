1702cd9f302c849c5b0c2faab98ef3ee1b517c58
Benjamin Eskola <ben@eskola.uk>
2022-11-16 13:45:54 +0000

    Insert cd into commandline when cding with keybinding so it's saved in history

--- fish/functions/__fzf_cd.fish
+++ fish/functions/__fzf_cd.fish
@@ -1,7 +1,11 @@
 function __fzf_cd --description 'Search the home directory using fzf and z. Change to the selected directory'
     set dir (z -l | awk '{print $2}' | fzf --preview 'exa -G {}' --cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*")
+
     if [ $status -eq 0 ]
-        z "$dir"
+        commandline -r "z "
+        commandline -a (string escape "$dir")
+        commandline -f execute
     end
+
     commandline -f repaint
 end

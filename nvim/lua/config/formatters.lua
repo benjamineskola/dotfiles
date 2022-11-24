8b4c66a528929e6341eb93f03a90164baed933c8
Benjamin Eskola <ben@eskola.uk>
2022-11-24 07:44:07 +0000

    Do additional Rome-based fixing if rome.json exists, otherwise Prettier

--- nvim/lua/config/formatters.lua
+++ nvim/lua/config/formatters.lua
@@ -5,6 +5,19 @@ local remove_trailing_newlines = {
     stdin = true,
 }
 
+local rome_lint = {
+    exe = "rome",
+    args = { "check", "--apply-suggested", util.escape_path(util.get_current_buffer_file_path()) },
+    ignore_exitcode = true,
+}
+
+local rome_or_prettier = function(filetype)
+    local utils = require("utils")
+    if utils.find_file_in_parent("rome.json", utils.get_git_root()) then return rome_lint end
+
+    return require("formatter.filetypes." .. filetype).prettierd
+end
+
 local erb_lint = {
     exe = "erblint",
     args = { "--autocorrect", "--stdin", util.escape_path(util.get_current_buffer_file_path()) },
@@ -25,11 +38,13 @@ require("formatter").setup({
         eruby = { erb_lint },
         fish = { require("formatter.filetypes.fish").fishindent },
         html = { require("formatter.filetypes.html").prettierd },
+        javascript = { rome_or_prettier("javascript") },
         json = { require("formatter.filetypes.json").prettierd },
         lua = { require("formatter.filetypes.lua").stylua },
         markdown = { require("formatter.filetypes.markdown").prettierd },
         scss = { require("formatter.filetypes.css").prettierd },
         sh = { require("formatter.filetypes.sh").shfmt },
+        typescript = { rome_or_prettier("typescript") },
         yaml = { require("formatter.filetypes.yaml").prettierd },
         ["*"] = {
             require("formatter.filetypes.any").remove_trailing_whitespace,

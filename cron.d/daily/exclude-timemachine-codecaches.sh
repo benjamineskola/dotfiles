2f12b3ed9fb83aebab29c88aedd1b685f4c21eef
Benjamin Eskola <ben@eskola.uk>
2022-11-27 09:43:10 +0000

    Don't try disabling time machine exclusions
    This was only checking ones listed through the UI, and failing. Exclusions added by this script expire if the directory is removed anyway.

--- cron.d/daily/exclude-timemachine-codecaches.sh
+++ cron.d/daily/exclude-timemachine-codecaches.sh
@@ -2,6 +2,3 @@
 set -e
 
 fd -HI -t d --prune '^(node_modules|dist-newstyle|\.stack-work|target|\.(mypy|pytest|sass)_cache)$' ~ -x tmutil addexclusion
-defaults read /Library/Preferences/com.apple.TimeMachine SkipPaths | sed -E 's/(^ *"|",?$)//g; s,^~ben,/Users/ben,' | grep '^/' | while read -r dir; do
-    test -d "$dir" || tmutil removeexclusion "$dir" 2>/dev/null || true
-done

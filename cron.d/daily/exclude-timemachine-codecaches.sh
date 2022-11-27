a4a1b80483709f2f58dd7387f733a9f066d76a8c
Benjamin Eskola <ben@eskola.uk>
2022-11-27 09:48:19 +0000

    Don't check Library/Caches for potential exclusions, it's already excluded

--- cron.d/daily/exclude-timemachine-codecaches.sh
+++ cron.d/daily/exclude-timemachine-codecaches.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-fd -HI -t d --prune '^(node_modules|dist-newstyle|\.stack-work|target|\.(mypy|pytest|sass)_cache)$' ~ -x tmutil addexclusion
+fd -E Library/Caches -HI -t d --prune '^(node_modules|dist-newstyle|\.stack-work|target|\.(mypy|pytest|sass)_cache)$' ~ -x tmutil addexclusion

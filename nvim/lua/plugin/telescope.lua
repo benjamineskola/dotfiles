e1ab1600840b41308773b12086b1984a8e08a654
Benjamin Eskola <ben@eskola.uk>
2022-11-27 17:42:42 +0000

    Use zoxide for my directory picker
    Allows priority sorting

--- nvim/lua/plugin/telescope.lua
+++ nvim/lua/plugin/telescope.lua
@@ -5,14 +5,15 @@ local actions = require("telescope.actions")
 M.projects = function()
     local opts = {
         find_command = {
-            "sh",
-            "-c",
-            "fd --type d --max-depth 1 . $HOME/Code $HOME/Code/Work $HOME/Code/Personal; echo $XDG_CONFIG_HOME",
+            "zoxide",
+            "query",
+            "-l",
         },
         attach_mappings = function(prompt_bufnr)
             actions.select_default:replace(function()
                 actions.close(prompt_bufnr)
                 local path = require("telescope.actions.state").get_selected_entry(prompt_bufnr).value
+                vim.fn.system("zoxide add " .. vim.fn.shellescape(path, true))
                 vim.cmd.cd(path)
             end)
             return true

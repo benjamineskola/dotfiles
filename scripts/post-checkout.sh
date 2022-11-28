82d67d3bacc37e1405198c3099c33376a2f02664
Benjamin Eskola <ben@eskola.uk>
2022-11-28 21:50:01 +0000

    Create LaunchAgents directory with ansible

--- scripts/post-checkout.sh
+++ scripts/post-checkout.sh
@@ -27,7 +27,6 @@ if [ "$(uname -s)" = Darwin ]; then
     tic -x tmux-256color
     rm -f tmux-256color
 
-    test -d LaunchAgents && mkdir -p ~/Library/LaunchAgents
     for i in LaunchAgents/*.plist; do
         launchctl unload "$HOME/Library/LaunchAgents/$(basename "$i")"
         sed -E "s,\\\$PATH,$PATH,g;  s,\\\$HOMEBREW_PREFIX,$HOMEBREW_PREFIX,g; s,\\\$HOME,$HOME,g; s,\\\$XDG_CONFIG_HOME,$XDG_CONFIG_HOME,g; s,\\\$XDG_DATA_HOME,$XDG_DATA_HOME,g; s,\\\$XDG_CACHE_HOME,$XDG_CACHE_HOME,g" "$HOME/.config/$i" >"$HOME/Library/LaunchAgents/$(basename "$i")"

cde8e034cf7fa3aef34edcb6e588ea15118633fb
Benjamin Eskola <ben@eskola.uk>
2022-11-28 21:56:16 +0000

    Install iterm scripts with ansible

--- scripts/post-checkout.sh
+++ scripts/post-checkout.sh
@@ -21,8 +21,6 @@ if [ "$(uname -s)" = Darwin ]; then
         curl https://iterm2.com/shell_integration/fish >"$HOME/.config/fish/conf.d/zzz_iterm2_shell_integration.fish"
     fi
 
-    ln -sf ~/.config/scripts/iterm* ~/Library/Application\ Support/iTerm2/Scripts/
-
     curl -O https://gist.githubusercontent.com/nicm/ea9cf3c93f22e0246ec858122d9abea1/raw/37ae29fc86e88b48dbc8a674478ad3e7a009f357/tmux-256color
     tic -x tmux-256color
     rm -f tmux-256color

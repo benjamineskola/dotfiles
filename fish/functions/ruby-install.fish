e7f50ff8effb6f7d925e16b1de636c655f57868c
Benjamin Eskola <ben@eskola.uk>
2022-11-29 19:29:43 +0000

    Better ruby-install command

--- fish/functions/ruby-install.fish
+++ fish/functions/ruby-install.fish
@@ -1,4 +1,8 @@
 function ruby-install --wraps=ruby-install
     set -l RUBY_TMP (mktemp -d)
-    command ruby-install -r "$RUBIES_DIR" -s "$RUBY_TMP" $argv
+    if brew --prefix capstone >/dev/null 2>&1
+        export CXXFLAGS="-I"(brew --prefix capstone)"/include"
+        export LDFLAGS="-L"(brew --prefix capstone)"/lib"
+    end
+    command ruby-install -r "$RUBIES_DIR" -s "$RUBY_TMP" $argv -- --enable-shared
 end

01c403d2ffc852634b0f89890781241a482a7389
Benjamin Eskola <ben@eskola.uk>
2022-11-29 09:16:05 +0000

    Update git repos from ansible

--- cron.d/daily/update.sh
+++ cron.d/daily/update.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 set -e
 
-for dir in ~/bin ~/.config; do
-    cd "$dir" || continue
-    git pull --rebase -q || true
-
-    cd
-done
-
 for req in ~/Code/*/requirements*.txt; do
     proj="$(dirname "$req")"
     test -f "$proj/.venv" || pip install -r "$req"

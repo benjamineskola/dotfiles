13f1509c29a482708acd7f008cd630961565c813
Benjamin Eskola <ben@eskola.uk>
2022-12-05 16:59:55 +0000

    Set terminal font size differently for different displays
    Slightly smaller font on lower-resolution (i.e. external) display

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,21 @@
 local wezterm = require("wezterm")
 
+local function resize_font(window)
+    local window_size = window:get_dimensions()
+    local overrides = window:get_config_overrides() or {}
+
+    if window_size.dpi < 100 then
+        overrides.font_size = 13
+    else
+        overrides.font_size = 14
+    end
+
+    window:set_config_overrides(overrides)
+end
+
+wezterm.on("window-resized", function(window, _) resize_font(window) end)
+wezterm.on("window-config-reloaded", function(window) resize_font(window) end)
+
 return {
     color_scheme = "tokyonight",
     font = wezterm.font({ family = "Fira Code", weight = "Light", harfbuzz_features = { "zero" } }),

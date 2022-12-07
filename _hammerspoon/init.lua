0c37a4a7cc403edcbe03906abed837b03646ea56
Benjamin Eskola <ben@eskola.uk>
2022-12-07 18:56:07 +0000

    Switch PDF Expert to and from dark mode automatically

--- _hammerspoon/init.lua
+++ _hammerspoon/init.lua
@@ -12,3 +12,16 @@ configWatcher = hs.pathwatcher
         hs.alert.show("Config loaded")
     end)
     :start()
+
+function applicationWatcher(appName, eventType, appObject)
+    if eventType == hs.application.watcher.activated then
+        if appName == "PDF Expert" then
+            local _, mode = hs.osascript.javascript('Application("System Events").appearancePreferences.darkMode.get()')
+            local modeName = mode and "Night" or "Day"
+
+            appObject:selectMenuItem({ "View", "Theme", modeName })
+        end
+    end
+end
+appWatcher = hs.application.watcher.new(applicationWatcher)
+appWatcher:start()

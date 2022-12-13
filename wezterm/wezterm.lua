2e09039b26e5be3dd6bcef23edbd3b52a820c1f8
Benjamin Eskola <ben@eskola.uk>
2022-12-13 13:52:02 +0000

    Refactor wezterm font loading

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,14 +1,18 @@
 local wezterm = require("wezterm")
 
+local font = { family = "Fira Code", weight = "Light", harfbuzz_features = { "zero" } }
+
 local function set_colour_scheme(window)
     local overrides = window:get_config_overrides() or {}
     local dark_mode = os.execute([[defaults read -globalDomain AppleInterfaceStyle]])
 
     if dark_mode then
         overrides.color_scheme = "tokyonight"
-        overrides.font = wezterm.font({ family = "Fira Code", weight = "Light", harfbuzz_features = { "zero" } })
+        local light_font = font
+        light_font.weight = "Light"
+        overrides.font = wezterm.font(light_font)
     else
-        overrides.font = wezterm.font({ family = "Fira Code", harfbuzz_features = { "zero" } })
+        overrides.font = wezterm.font(font)
         overrides.color_scheme = "tokyonight-day"
     end
 
@@ -36,7 +40,7 @@ wezterm.on("window-config-reloaded", function(window) set_colour_scheme(window) 
 
 return {
     color_scheme = "tokyonight",
-    font = wezterm.font({ family = "Fira Code", weight = "Light", harfbuzz_features = { "zero" } }),
+    font = wezterm.font(font),
     font_size = 14,
     native_macos_fullscreen_mode = true,
     send_composed_key_when_left_alt_is_pressed = false,

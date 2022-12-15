ac8070adaa2d5b5ddad46a88b22c997cd73bddaf
Benjamin Eskola <ben@eskola.uk>
2022-12-15 11:46:26 +0000

    lint

--- _hammerspoon/init.lua
+++ _hammerspoon/init.lua
@@ -1,19 +1,18 @@
-function reloadConfig(files)
-    doReload = false
+local function reloadConfig(files)
+    local doReload = false
     for _, file in pairs(files) do
         if file:sub(-4) == ".lua" then doReload = true end
     end
     if doReload then hs.reload() end
 end
-configWatcher = hs.pathwatcher
-    .new(os.getenv("HOME") .. "/.hammerspoon/", function(files)
-        reloadConfig(files)
+local configWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", function(files)
+    reloadConfig(files)
 
-        hs.alert.show("Config loaded")
-    end)
-    :start()
+    hs.alert.show("Config loaded")
+end)
+configWatcher:start()
 
-function applicationWatcher(appName, eventType, appObject)
+local function applicationWatcher(appName, eventType, appObject)
     if eventType == hs.application.watcher.activated then
         if appName == "PDF Expert" then
             local _, mode = hs.osascript.javascript('Application("System Events").appearancePreferences.darkMode.get()')
@@ -23,5 +22,5 @@ function applicationWatcher(appName, eventType, appObject)
         end
     end
 end
-appWatcher = hs.application.watcher.new(applicationWatcher)
+local appWatcher = hs.application.watcher.new(applicationWatcher)
 appWatcher:start()

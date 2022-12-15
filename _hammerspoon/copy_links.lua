fd7ac5d3e045806f3b42b79d477cc77b2d05877e
Benjamin Eskola <ben@eskola.uk>
2022-12-15 12:44:01 +0000

    Refactor linking

--- _hammerspoon/copy_links.lua
+++ _hammerspoon/copy_links.lua
@@ -1,38 +1,47 @@
-local M = {
-    copy_link = {
-        NetNewsWire = function()
-            local _, result = hs.osascript.javascript("Application('NetNewsWire').currentArticle.url()")
-            return result
-        end,
-        Safari = function()
-            local _, result = hs.osascript.javascript("Application('Safari').documents[0].url()")
-            return result
-        end,
-    },
-    markdown_link = {
-        NetNewsWire = function()
-            local _, url = hs.osascript.javascript("Application('NetNewsWire').currentArticle.url()")
-            local _, title = hs.osascript.javascript("Application('NetNewsWire').currentArticle.title()")
-            return "[" .. title .. "](" .. url .. ")"
-        end,
-        Safari = function()
-            local _, url = hs.osascript.javascript("Application('Safari').documents[0].url()")
-            local _, title = hs.osascript.javascript("Application('Safari').documents[0].name()")
-            return "[" .. title .. "](" .. url .. ")"
-        end,
-    },
+local M = {}
+
+M.get_url = {
+    NetNewsWire = function()
+        local _, result = hs.osascript.javascript("Application('NetNewsWire').currentArticle.url()")
+        return result
+    end,
+    Safari = function()
+        local _, result = hs.osascript.javascript("Application('Safari').documents[0].url()")
+        return result
+    end,
+}
+
+M.get_title = {
+    NetNewsWire = function()
+        local _, title = hs.osascript.javascript("Application('NetNewsWire').currentArticle.title()")
+        return title
+    end,
+    Safari = function()
+        local _, title = hs.osascript.javascript("Application('Safari').documents[0].name()")
+        return title
+    end,
 }
 
 hs.hotkey.bind({ "cmd", "shift" }, "c", function()
     local win = hs.window.focusedWindow()
     local app_name = win:application():name()
-    if M.copy_link[app_name] then hs.pasteboard.setContents(M.copy_link[app_name]()) end
+    if M.get_url[app_name] then hs.pasteboard.setContents(M.get_url[app_name]()) end
 end)
 
 hs.hotkey.bind({ "cmd", "shift" }, "m", function()
     local win = hs.window.focusedWindow()
     local app_name = win:application():name()
-    if M.markdown_link[app_name] then hs.pasteboard.setContents(M.markdown_link[app_name]()) end
+    if M.get_url[app_name] then
+        if M.get_title[app_name] then
+            local url = M.get_url[app_name]()
+            local title = M.get_title[app_name]()
+            if url and title then
+                hs.pasteboard.setContents("[" .. title .. "](" .. url .. ")")
+                return
+            end
+        end
+        hs.pasteboard.setContents(M.get_url[app_name]())
+    end
 end)
 
 return M

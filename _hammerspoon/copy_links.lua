cd66bcd0c3b1b7ec1d1a1eb9c39c80ce9bddf361
Benjamin Eskola <ben@eskola.uk>
2022-12-16 15:42:07 +0000

    Add Bear support to the copy-link function

--- _hammerspoon/copy_links.lua
+++ _hammerspoon/copy_links.lua
@@ -1,6 +1,19 @@
 local M = {}
 
+local bear_token = hs.json.read(os.getenv("XDG_CONFIG_HOME") .. "/private/hs-bear.conf").token
+local function get_current_bear_note(link)
+    local bear = require("Bear")
+    bear.init(bear_token)
+    local note = bear:getCurrent()
+    if link then note.link = bear:getLink(note.identifier) end
+    return note
+end
+
 M.get_url = {
+    Bear = function()
+        local note = assert(get_current_bear_note(true))
+        return note.link
+    end,
     NetNewsWire = function()
         local _, result = hs.osascript.javascript("Application('NetNewsWire').currentArticle.url()")
         return result
@@ -12,6 +25,10 @@ M.get_url = {
 }
 
 M.get_title = {
+    Bear = function()
+        local note = assert(get_current_bear_note())
+        return note.title
+    end,
     NetNewsWire = function()
         local _, title = hs.osascript.javascript("Application('NetNewsWire').currentArticle.title()")
         return title
@@ -25,7 +42,10 @@ M.get_title = {
 hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "c", function()
     local win = hs.window.focusedWindow()
     local app_name = win:application():name()
-    if M.get_url[app_name] then hs.pasteboard.setContents(M.get_url[app_name]()) end
+    if M.get_url[app_name] then
+        local url = M.get_url[app_name]()
+        if url then hs.pasteboard.setContents(url) end
+    end
 end)
 
 hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "m", function()

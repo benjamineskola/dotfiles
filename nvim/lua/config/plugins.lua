c1032ea82744e5240057af21c8fdae88c8bbed1c
Benjamin Eskola <ben@eskola.uk>
2022-12-21 19:04:42 +0000

    Move completion configuration into new (lazy.nvim) framework

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -27,17 +27,6 @@ return {
         config = function() require("config.wilder") end,
     },
     {
-        "hrsh7th/nvim-cmp",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-
-        "saadparwaiz1/cmp_luasnip",
-        "L3MON4D3/LuaSnip",
-        "rafamadriz/friendly-snippets",
-    },
-    {
         "jose-elias-alvarez/typescript.nvim",
         ft = { "typescript" },
         config = function() require("typescript").setup({}) end,

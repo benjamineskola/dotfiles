f8647037feefc510bd357daf241351164ba15043
Benjamin Eskola <ben@eskola.uk>
2022-12-21 19:15:11 +0000

    Don't keep indent-blankline config separately

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -52,7 +52,16 @@ return {
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function() require("config.gitsigns") end,
     },
-    { "lukas-reineke/indent-blankline.nvim" },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        config = function()
+            require("indent_blankline").setup({
+                space_char_blankline = " ",
+                show_current_context = true,
+                show_current_context_start = true,
+            })
+        end,
+    },
     {
         "lukas-reineke/virt-column.nvim",
         config = function() require("virt-column").setup() end,

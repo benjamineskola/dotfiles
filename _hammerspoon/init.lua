0c0e823f94bdc2e7605c5ae33a6dcddc8d3df225
Benjamin Eskola <ben@eskola.uk>
2023-01-03 16:15:32 +0000

    Implement noclamshell functionality in hammerspoon

--- _hammerspoon/init.lua
+++ _hammerspoon/init.lua
@@ -15,3 +15,20 @@ local function applicationWatcher(appName, eventType, appObject)
 end
 local appWatcher = hs.application.watcher.new(applicationWatcher)
 appWatcher:start()
+
+local screen_watcher = hs.screen.watcher.new(function()
+    local logger = hs.logger.new("noclamshell")
+    logger.df("Received hs.screen.watcher event")
+    local screens = hs.screen.allScreens()
+    logger.df("  Screens: %s", hs.inspect(screens))
+    if #screens > 0 and hs.fnutils.every(screens, function(s) return s:name() ~= "Color LCD" end) then
+        logger.df("  'Color LCD' display is gone but other screens remain - detecting this as 'lid close'")
+        -- the following check is from pirj/noclamshell to query the actual state
+        local lid_closed = os.execute("ioreg -r -k AppleClamshellState | grep AppleClamshellState | grep Yes")
+        if lid_closed then
+            hs.alert("detected lid closed the hard way")
+            os.execute("pmset sleepnow")
+        end
+    end
+end)
+screen_watcher:start()

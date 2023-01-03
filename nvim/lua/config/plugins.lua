76f79a3f2a902be3629538ad77d33d6e8951f116
Benjamin Eskola <ben@eskola.uk>
2023-01-03 09:59:19 +0000

    Add yanky plugin

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -23,6 +23,19 @@ return {
         end,
     },
     {
+        "gbprod/yanky.nvim",
+        event = { "VeryLazy" },
+        config = true,
+        init = function()
+            vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
+            vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
+            vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
+            vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
+            vim.keymap.set("n", "<c-n>", "<Plug>(YankyCycleForward)")
+            vim.keymap.set("n", "<c-p>", "<Plug>(YankyCycleBackward)")
+        end,
+    },
+    {
         "jose-elias-alvarez/typescript.nvim",
         ft = { "typescript" },
         config = true,

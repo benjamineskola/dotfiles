160042581ce317e655c90cde123bdc569c2aa25a
Benjamin Eskola <ben@eskola.uk>
2023-01-06 21:25:56 +0000

    Take colorcolumn for rust files from rustfmt

--- nvim/ftplugin/rust.lua
+++ nvim/ftplugin/rust.lua
@@ -1,2 +1,9 @@
+vim.b.colorcolumn = tonumber(
+    vim.fn.system(
+        [[rustfmt --print-config current ]]
+            .. vim.fn.shellescape(vim.api.nvim_buf_get_name(0))
+            .. [[ 2>/dev/null | grep '^max_width ' | cut -d '=' -f 2]]
+    )
+)
 vim.bo.expandtab = true
 vim.bo.shiftwidth = 4

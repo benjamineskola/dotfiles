f92f3d3da2154dc25eb1b2eab1733636f5f90dac
Benjamin Eskola <ben@eskola.uk>
2023-02-01 11:40:34 +0000

    Don't duplicate ansible runs

--- cron.d/hourly/ansible.sh
+++ cron.d/hourly/ansible.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+pgrep -qf ansible-playbook && exit
 export ANSIBLE_HOME="$XDG_CONFIG_HOME/ansible"
 export ANSIBLE_CONFIG="$ANSIBLE_HOME/ansible.cfg"
 ansible-galaxy install -r "$ANSIBLE_HOME/requirements.yaml"

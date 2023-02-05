24bc813c88c59c2ee43d79d3e7d26ca7bc187052
Benjamin Eskola <ben@eskola.uk>
2023-02-05 17:44:26 +0000

    Add strava-dayone cronjob

---
+++ cron.d/hourly/strava2dayone.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd ~/Code/strava-reporter || exit 1
+pgrep -lf dayone.py && exit 1
+
+# shellcheck disable=SC1091
+. /Users/ben/Library/virtualenvs/strava-reporter/bin/activate
+./dayone.py run
+./dayone.py ride
+./dayone.py walk

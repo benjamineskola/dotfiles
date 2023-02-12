be768e5bc075edfcb8984015426fd7ab4d027eee
Benjamin Eskola <ben@eskola.uk>
2023-02-12 15:37:46 +0000

    Make strava cronjob more robust

--- cron.d/hourly/strava2dayone.sh
+++ cron.d/hourly/strava2dayone.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+set -e
+
 cd ~/Code/strava-reporter || exit 1
-pgrep -lf dayone.py && exit 1
+pgrep -lf /dayone.py && exit 1
 
 # shellcheck disable=SC1091
 . /Users/ben/Library/virtualenvs/strava-reporter/bin/activate

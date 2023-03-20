810d21e61cbf03e37d4aca1d8d0fc5ddae667b18
Benjamin Eskola <ben@eskola.uk>
2023-03-20 09:06:20 +0000

    Remove iterm

--- cron.d/daily/exclude-timemachine-codecaches.sh
+++ cron.d/daily/exclude-timemachine-codecaches.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 set -e
 
-tmutil addexclusion ~/Library/Application\ Support/iTerm2/iterm2env*
-
 fd -E Library/Caches -HI -t d --prune '^(node_modules|dist-newstyle|\.stack-work|target|\.(mypy|pytest|sass)_cache)$' ~ -x tmutil addexclusion

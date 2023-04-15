05483bd21127c09d0f3112c71ba0c792e3ad042a
Benjamin Eskola <ben@eskola.uk>
2023-04-15 09:38:35 +0100

    Create provisioner framework in place of ansible

---
+++ scripts/provision.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -e
+# set -o pipefail
+
+VERBOSE=0
+
+case "$1" in
+-v | --verbose)
+    VERBOSE=1
+    ;;
+-d | --debug)
+    VERBOSE=2
+    ;;
+esac
+
+if [ "$VERBOSE" -gt 0 ]; then
+    log() {
+        printf "==> %s [%s]\n" "$SCRIPT_NAME" "$@" >&2
+    }
+else
+    log() { :; }
+fi
+
+if [ "$VERBOSE" -gt 1 ]; then
+    set -x
+fi
+
+for script in "$XDG_CONFIG_HOME"/scripts/provision.d/*.sh; do
+    SCRIPT_NAME=$(basename "$script" .sh)
+    export SCRIPT_NAME
+    if [ "$VERBOSE" -gt 1 ]; then
+        # shellcheck source=/dev/null
+        . "$script"
+    else
+        # shellcheck source=/dev/null
+        . "$script" >/dev/null
+    fi
+done

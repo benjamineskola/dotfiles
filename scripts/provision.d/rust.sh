c52bc33ab9c99c916d45c36a0c785574b79e8d5c
Benjamin Eskola <ben@eskola.uk>
2023-04-15 10:22:43 +0100

    Move Rust from ansible to provision.sh

---
+++ scripts/provision.d/rust.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+if ! [ -e "$CARGO_HOME/bin/rustup" ]; then
+    log "Initialise rustup"
+    rustup-init --no-update-default-toolchain --no-modify-path -y
+fi
+
+log "Set default rust version to stable"
+"$CARGO_HOME/bin/rustup" default stable
+
+log "Additionally install nightly"
+"$CARGO_HOME/bin/rustup" install nightly
+
+log "Use nightly for specific components"
+# shellcheck disable=SC2043
+for item in rustfmt; do
+    "$CARGO_HOME/bin/rustup" component add "$item" --toolchain nightly
+done

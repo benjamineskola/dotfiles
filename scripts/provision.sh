7d2c1d275ba9bdec39d63e161881a0ee20af53a8
Benjamin Eskola <ben@eskola.uk>
2023-04-16 19:54:23 +0100

    Refactor script loading

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -44,32 +44,36 @@ ln_relative() {
     ln -sfh "$(relpath "$(dirname "$2")" "$(realpath "$1")")" "$2"
 }
 
-for script in "$XDG_CONFIG_HOME"/scripts/provision.d/*.sh; do
-    SCRIPT_NAME=$(basename "$script" .sh)
-    export SCRIPT_NAME
-
-    if [ -n "$ONLY" ] && [ "$ONLY" != "$SCRIPT_NAME" ]; then
-        continue
-    fi
+run_script() {
+    SCRIPT_PATH=$1
+    SCRIPT_NAME=$(basename "$SCRIPT_PATH" .sh)
 
     case "$SCRIPT_NAME" in
     os-"$(uname -s | tr "[:upper:]" "[:lower:]")")
-        SCRIPT_NAME=$(uname -s | tr "[:upper:]" "[:lower:]")
+        SCRIPT_NAME=${SCRIPT_NAME#os-}
         ;;
-    os-*) continue ;;
+    os-*) return ;;
     esac
 
     start=$(date +%s)
     if [ "$VERBOSE" -gt 1 ]; then
         # shellcheck source=/dev/null
-        . "$script"
+        . "$SCRIPT_PATH"
     else
         # shellcheck source=/dev/null
-        . "$script" >/dev/null
+        . "$SCRIPT_PATH" >/dev/null
     fi
     end=$(date +%s)
 
     if [ "$VERBOSE" -gt 0 ]; then
         log "done in $((end - start)) seconds"
     fi
-done
+}
+
+if [ -n "$ONLY" ]; then
+    run_script "$XDG_CONFIG_HOME/scripts/provision.d/$ONLY.sh"
+else
+    for script in "$XDG_CONFIG_HOME"/scripts/provision.d/*.sh; do
+        run_script "$script"
+    done
+fi

98fa5a901a16f5ea4daa0fe81b93096eda4fac5a
Benjamin Eskola <ben@eskola.uk>
2023-04-17 09:20:32 +0100

    Only run homebrew if current user owns it

--- scripts/provision.d/os-darwin.sh
+++ scripts/provision.d/os-darwin.sh
@@ -5,7 +5,7 @@ log "Don't automatically rearrange spaces"
 defaults write com.apple.dock mru-spaces -bool false
 
 log "Update Homebrew and install required packages"
-if [ -z "$(find /opt/homebrew -type f -mtime -1 | head -n 1)" ]; then
+if [ -O /opt/homebrew ] && [ -z "$(find /opt/homebrew -type f -mtime -1 | head -n 1)" ]; then
     brew update
     brew upgrade
     brew bundle --global install

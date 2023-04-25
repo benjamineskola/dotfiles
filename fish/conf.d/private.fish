fd6ac297401e17e24acf07ffd83d84e1c913ea00
Benjamin Eskola <ben@eskola.uk>
2023-04-25 14:26:20 +0100

    Switch back to Zsh

--- fish/conf.d/private.fish
+++
@@ -1,3 +0,0 @@
-for file in $XDG_CONFIG_HOME/private/fish/*.fish
-    builtin source $file 2>/dev/null
-end
--- fish/functions/ruby-install.fish
+++
@@ -1,8 +0,0 @@
-function ruby-install --wraps=ruby-install
-    set -l RUBY_TMP (mktemp -d)
-    if brew --prefix capstone >/dev/null 2>&1
-        export CXXFLAGS="-I"(brew --prefix capstone)"/include"
-        export LDFLAGS="-L"(brew --prefix capstone)"/lib"
-    end
-    command ruby-install -r "$RUBIES_DIR" -s "$RUBY_TMP" $argv -- --enable-shared
-end

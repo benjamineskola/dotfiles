847b81429fd2c20c3288c84acbf632c64f36e14b
Benjamin Eskola <ben@eskola.uk>
2023-05-11 10:14:13 +0100

    Enable kitty extended keyboard mode

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -20,6 +20,7 @@ wezterm.on("window-config-reloaded", resize_font)
 
 return {
     color_scheme = "Monokai Pro (Gogh)",
+    enable_kitty_keyboard = true,
     font = wezterm.font(font),
     font_size = 14,
     native_macos_fullscreen_mode = true,

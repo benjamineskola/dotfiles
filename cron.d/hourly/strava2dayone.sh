20b15baaed155eb04b0eeac50c566f860d7a5dc8
Benjamin Eskola <ben@eskola.uk>
2023-06-25 19:45:57 +0100

    Make strava cronjob more reliable

--- cron.d/hourly/strava2dayone.sh
+++ cron.d/hourly/strava2dayone.sh
@@ -2,13 +2,20 @@
 set -e
 
 cd ~/Code/strava-reporter || exit 1
-pgrep -lf /dayone.py && exit 1
+pkill -f dayone.py || true
 
 PYTHON=python
 if [ -d ./.venv ]; then
 	PYTHON=$PWD/.venv/bin/python
 fi
 
-$PYTHON ./dayone.py run
-$PYTHON ./dayone.py ride
-$PYTHON ./dayone.py walk
+$PYTHON ./dayone.py run &
+$PYTHON ./dayone.py ride &
+$PYTHON ./dayone.py walk &
+
+for _ in $(seq 30); do
+	pgrep -lf dayone.py || exit
+	sleep 10
+done
+
+pkill -f dayone.py
